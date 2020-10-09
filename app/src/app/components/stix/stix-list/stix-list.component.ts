68c56c3baf79c7056e46efeccdc7ec1da0901d85
Isabel Tuson <isabel@mitre.org>
2020-10-09 12:30:28 -0400

    update stix-list to a table

--- app/src/app/components/stix/stix-list/stix-list.component.ts
+++ app/src/app/components/stix/stix-list/stix-list.component.ts
@@ -3,14 +3,27 @@ import { StixObject } from 'src/app/classes/stix/stix-object';
 import { CollectionService } from 'src/app/services/stix/collection/collection.service';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { NestedTreeControl } from '@angular/cdk/tree';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {MatSort} from '@angular/material/sort';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-stix-list',
   templateUrl: './stix-list.component.html',
   styleUrls: ['./stix-list.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('detailExpand', [
+        state('collapsed', style({height: '0px', minHeight: '0'})),
+        state('expanded', style({height: '*'})),
+        transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      ]),
+  ]
 })
 export class StixListComponent implements OnInit {
+
+    @Input() public relatedTo: String = null;
+    @Input() public type: String = null
 
     @Input() public stixObjects: StixObject[]; //TODO get rid of this in favor of stix list cards loading using filters
     @Input() public showOnly: StixListConfig = {};
@@ -23,16 +36,24 @@ export class StixListComponent implements OnInit {
     public groupBy: string[] = [];
     // search query
     public query: string = "";
+
+    // TABLE STUFF
+    public tableColumns: string[] = [
+        "name", 
+        "attackType",
+        "version"
+    ]
+    public expandedElement: StixObject | null;
+    // @ViewChild(MatSort) public sort: MatSort;
+    // @ViewChild(MatPaginator) public paginator: MatPaginator;
+
     
     
-    //dataSource for the tree
-    private dataSource = new MatTreeNestedDataSource<StixListNode>();
-    //control for node visibility for the tree
-    private treeControl = new NestedTreeControl<StixListNode>(node => node.children);
-    //trackBy function for the tree
-    private treeNodeTrackBy(index, item: StixListNode) {
-        return item.name;
-    }
+    
+
+
+
+
 
     //all possible each type of filter/groupBy
     private types: FilterValue[] = [
@@ -57,170 +78,6 @@ export class StixListComponent implements OnInit {
     ]
 
     constructor(private collectionService: CollectionService) {}
-
-    public generateSections() {
-        // parse filters into StixListConfig objects
-        let parsedFilters: StixListConfig[] = [];
-        for (let filter of this.filter) {
-            let filterType = filter.split(".")[0];
-            let filterConfig: StixListConfig = {}
-            filterConfig[filterType] = filter;
-            parsedFilters.push(filterConfig);
-        }
-
-        this.dataSource.data = null; //for some reason if you don't first set it to null Angular doesn't see a change
-        if (this.groupBy.includes("type")) this.dataSource.data = this.generateTypeSections(parsedFilters);
-        else this.dataSource.data = this.generateSubsection("type", parsedFilters);
-    }
-    // does the given node have children?
-    private hasChildren(index, node) { return !!node.children && node.children.length > 0}
-
-    /**
-     * generate the next subsection of the current section
-     * @param {string} currentSection: name of the current section
-     * @param {StixListConfig[]} parentFilters: filters inherited from parent sections
-     * @returns {StixListNode[]} node tree for this subsection and all child subsections
-     */
-    private generateSubsection(currentSection: string, parentFilters: StixListConfig[]): StixListNode[] {
-        if (currentSection == "type") {
-            if (!this.groupBy.includes("domain")) {
-                return this.generateSubsection("domain", parentFilters);
-            } else {
-                return this.generateDomainSections(parentFilters);
-            }
-        } else if (currentSection == "domain") {
-            if (!this.groupBy.includes("collection")) {
-                return this.generateSubsection("collection", parentFilters);
-            } else {
-                return this.generateCollectionSections(parentFilters);
-            }
-        } else if (currentSection == "collection") {
-            if (this.groupBy.includes("status")) {
-                return this.generateStatusSections(parentFilters);
-            } else return []; //else don't skip because there's nothing else
-        }
-    }
-    /**
-     * generate sections for each type, and child sections
-     * @param {StixListConfig[]} parentFilters filters inherited from parent sections
-     * @returns {StixListNode[]} node tree for this subsection and all child subsections
-     */
-    private generateTypeSections(parentFilters: StixListConfig[]): StixListNode[] {
-        let configs: StixListNode[] = []
-        for (let thetype of this.types) {
-            // check if this type is filtered
-            let filtered = false;
-            for (let filter of parentFilters) {
-                if (filter.type && filter.type == thetype.value) {
-                    filtered = false;
-                    break;
-                } else if (filter.type) { filtered = true; }
-            }
-
-            if (filtered) continue;
-
-            // add the type
-            //remove other filters for type because we only want this type for descendants
-            let filters = parentFilters.filter((filter: StixListConfig) => !filter.type).concat([{"type": thetype.value as type_attacktype}]);
-            configs.push({
-                name: thetype.label,
-                filters: filters,
-                children: this.generateSubsection("type", filters)
-            })
-        }
-        return configs;
-    }
-    /**
-     * generate sections for each domain, and child sections
-     * @param {StixListConfig[]} parentFilters filters inherited from parent sections
-     * @returns {StixListNode[]} node tree for this subsection and all child subsections
-     */
-    private generateDomainSections(parentFilters: StixListConfig[]): StixListNode[] {
-        let configs: StixListNode[] = []
-        for (let domain of this.domains) {
-            // check if this domain is filtered
-            let filtered = false;
-            for (let filter of parentFilters) {
-                if (filter.domain && filter.domain == domain.value) {
-                    filtered = false;
-                    break;
-                } else if (filter.domain) { filtered = true; }
-            }
-
-            if (filtered) continue;
-
-            //add the domain
-            //remove other filters for domain because we only want this domain for descendants
-            let filters = parentFilters.filter((filter: StixListConfig) => !filter.domain).concat([{"domain": domain.value as type_domain}]);
-            configs.push({
-                name: domain.label,
-                filters: filters,
-                children: this.generateSubsection("domain", filters)
-            })
-        }
-        return configs;
-    }
-
-    /**
-     * generate sections for each collection, and child sections
-     * @param {StixListConfig[]} parentFilters filters inherited from parent sections
-     * @returns {StixListNode[]} node tree for this subsection and all child subsections
-     */
-    private generateCollectionSections(parentFilters: StixListConfig[]): StixListNode[] {
-        let configs: StixListNode[] = []
-        for (let collection of this.collections) {
-            // check if this collection is filtered
-            let filtered = false;
-            for (let filter of parentFilters) {
-                if (filter.collection && filter.collection == collection.value) {
-                    filtered = false;
-                    break;
-                } else if (filter.collection) { filtered = true; }
-            }
-
-            if (filtered) continue;
-
-            //add the collection
-            //remove other filters for collection because we only want this collection for descendants
-            let filters = parentFilters.filter((filter: StixListConfig) => !filter.collection).concat([{"collection": collection.value}]);
-            configs.push({
-                name: collection.label,
-                filters: filters,
-                children: this.generateSubsection("collection", filters)
-            })
-        }
-        return configs;
-    }
-
-    /**
-     * generate sections for each status, and child sections
-     * @param {StixListConfig[]} parentFilters filters inherited from parent sections
-     * @returns {StixListNode[]} node tree for this subsection and all child subsections
-     */
-    private generateStatusSections(parentFilters: StixListConfig[]): StixListNode[] {
-        let configs: StixListNode[] = []
-        for (let status of this.statuses) {
-            // check if this status is filtered
-            let filtered = false;
-            for (let filter of parentFilters) {
-                if (filter.status && filter.status == status.value) {
-                    filtered = false;
-                    break;
-                } else if (filter.status) { filtered = true; }
-            }
-
-            if (filtered) continue;
-
-            //add the status
-            //remove other filters for status because we only want this status for descendants
-            configs.push({
-                name: status.label,
-                filters: parentFilters.filter((filter: StixListConfig) => !filter.status).concat([{"status": status.value as type_status}]),
-                children: []
-            })
-        }
-        return configs;
-    }
 
     ngOnInit() {
         this.collections = this.collectionService.getAll().map((collection) => {return {"value": "collection." + collection.stixID, "label": collection.name}})
@@ -261,7 +118,6 @@ export class StixListComponent implements OnInit {
             })
             if (this.groupBy.length == 0) this.groupBy = ["status"];
         }
-        this.generateSections();
     }
 }
 
