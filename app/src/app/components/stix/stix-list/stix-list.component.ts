e9956e48dc2357dc6953807eab5afb1ba8d1ba57
Isabel Tuson <isabel@mitre.org>
2020-10-13 13:36:57 -0400

    loading animations for stix-list

--- app/src/app/components/stix/stix-list/stix-list.component.ts
+++ app/src/app/components/stix/stix-list/stix-list.component.ts
@@ -33,6 +33,12 @@ import { SelectionModel } from '@angular/cdk/collections';
             transition(':leave', [
                 animate('100ms cubic-bezier(0.4, 0.0, 0.2, 1)', style({ height: '0px', minHeight: '0px' }))
             ])
+        ]),
+        trigger("fadeIn", [
+            transition(":enter", [
+                style({ opacity: 0 }),
+                animate("500ms cubic-bezier(0.4, 0.0, 0.2, 1)", style({opacity: '1'}))
+            ])
         ])
     ]
 })
