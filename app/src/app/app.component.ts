8f515ff8c30a185d4c5710311b40276f090b1e6a
Isabel Tuson <isabel@mitre.org>
2020-10-29 13:51:11 -0400

    header now hides itself when scroll of main container is not top

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatDrawerContainer } from '@angular/material/sidenav';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { getCookie, setCookie, hasCookie } from "./util/cookies";
@@ -8,9 +8,13 @@ import { getCookie, setCookie, hasCookie } from "./util/cookies";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     //drawer container to resize when contents change size
-    @ViewChild(MatDrawerContainer, {static: true}) container: MatDrawerContainer;
+    @ViewChild(MatDrawerContainer, {static: true}) private container: MatDrawerContainer;
+    //elements for scroll behavior
+    @ViewChild("header", {static: false, read: ElementRef}) private header: ElementRef;
+    @ViewChild("scrollRef", {static: false, read: ElementRef}) private scrollRef: ElementRef;
+
     constructor(private overlayContainer: OverlayContainer) {
         if (hasCookie("theme")) {
             this.setTheme(getCookie("theme"))
@@ -33,13 +37,29 @@ export class AppComponent {
         overlayContainerClasses.add(this.theme);
         setCookie("theme", theme, 30);
     }
-    
-    // public showSidebar: boolean = false;
-    // public toggleSidebar() {
-    //     this.
-    // }
 
-    resizeDrawers() {
+    // header hiding with scroll
+    ngAfterViewInit() {
+        this.scrollRef.nativeElement.addEventListener('scroll', (e) => this.scrollEvent(e), true);
+    }
+    ngOnDestroy() { 
+        this.scrollRef.nativeElement.removeEventListener('scroll', (e) => this.scrollEvent(e), true);
+    }
+
+    public hiddenHeaderPX: number = 0; //number of px of the header which is hidden
+    // when a scroll happens
+    private scrollEvent($event): void {
+        let headerHeight = this.header.nativeElement.offsetHeight;
+        // constrain amount of hidden to bounds
+        this.hiddenHeaderPX = Math.min(Math.max(0, this.scrollRef.nativeElement.scrollTop/2), headerHeight); 
+    }
+    // scroll to the top of the main content
+    public scrollToTop(): void {
+        this.scrollRef.nativeElement.scroll({top: 0, behavior: "smooth"});
+    }
+
+    // when the drawer resizes
+    public resizeDrawers(): void {
         console.log("resizing drawer")
         this.container.updateContentMargins();
     }
