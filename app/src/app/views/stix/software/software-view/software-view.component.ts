da22729c392e0ecb87c819093d35525d17797b70
Isabel Tuson <isabel@mitre.org>
2020-10-30 10:54:52 -0400

    mock up software view page

--- app/src/app/views/stix/software/software-view/software-view.component.ts
+++ app/src/app/views/stix/software/software-view/software-view.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Group } from 'src/app/classes/stix/group';
+import { Software } from 'src/app/classes/stix/software';
+import { Technique } from 'src/app/classes/stix/technique';
+import { GroupService } from 'src/app/services/stix/group/group.service';
+import { SoftwareService } from 'src/app/services/stix/software/software.service';
+import { TechniqueService } from 'src/app/services/stix/technique/technique.service';
 
 @Component({
   selector: 'app-software-view',
@@ -6,10 +13,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./software-view.component.scss']
 })
 export class SoftwareViewComponent implements OnInit {
+    public groups: Group[];
+    public techniques: Technique[];
 
-  constructor() { }
+    constructor(private groupService: GroupService, private techniqueService: TechniqueService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.groups = this.groupService.getAll();
+        this.techniques = this.techniqueService.getAll();
+    }
 
 }
