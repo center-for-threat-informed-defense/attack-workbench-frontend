030b976669c98a06c2d15c718fbf24a2e64f9e10
Isabel Tuson <isabel@mitre.org>
2020-11-10 09:44:45 -0500

    made mode config optional for version property config

--- app/src/app/components/stix/version-property/version-property.component.ts
+++ app/src/app/components/stix/version-property/version-property.component.ts
@@ -17,12 +17,12 @@ export class VersionPropertyComponent implements OnInit {
 }
 
 export interface VersionPropertyConfig {
-    /* What is the current mode?
+    /* What is the current mode? Default: 'view
      *    view: viewing the version property
      *    edit: editing the version property
      *    diff: displaying the diff between two STIX objects. If this mode is selected, two StixObjects must be specified in the objects field
      */
-    mode: "view" | "edit" | "diff";
+    mode?: "view" | "edit" | "diff";
     /* The object to show the version of
      * Note: if mode is diff, pass an array of two objects to diff
      */
