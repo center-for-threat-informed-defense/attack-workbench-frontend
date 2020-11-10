057a099ac07315077a5794ad8e901cfa5e7b4a69
Isabel Tuson <isabel@mitre.org>
2020-11-10 09:42:49 -0500

    mock up editing settings; version component

--- app/src/app/components/stix/version-property/version-view/version-view.component.ts
+++ app/src/app/components/stix/version-property/version-view/version-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { VersionPropertyConfig } from '../version-property.component';
 
 @Component({
   selector: 'app-version-view',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./version-view.component.scss']
 })
 export class VersionViewComponent implements OnInit {
+    @Input() public config: VersionPropertyConfig;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
