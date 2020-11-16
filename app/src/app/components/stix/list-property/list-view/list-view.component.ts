da6f563bfdc93ae9ee1ea9dfcb73ad9da72e8f34
Isabel Tuson <isabel@mitre.org>
2020-11-16 13:53:37 -0500

    initial version of list-view feature

--- app/src/app/components/stix/list-property/list-view/list-view.component.ts
+++ app/src/app/components/stix/list-property/list-view/list-view.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { ListPropertyConfig } from '../list-property.component';
 
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
-  styleUrls: ['./list-view.component.scss']
+  styleUrls: ['./list-view.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ListViewComponent implements OnInit {
+    @Input() public config: ListPropertyConfig;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
