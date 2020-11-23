0be3ac39a697b21f1662b8c988587da7fcab3185
idavila <idavila>
2020-11-23 17:18:17 -0500

    PR review changes

--- app/src/app/classes/external-references.ts
+++ app/src/app/classes/external-references.ts
@@ -18,7 +18,7 @@ export class ExternalReferences {
         // Restart _externalReferencesIndex map
         this._externalReferencesIndex = new Map();
         // Start index at 1
-        var index = 1;
+        let index = 1;
         // Index 1
         for (let [key, value] of this._externalReferences) {
             // Add to map if it has a description
@@ -31,14 +31,14 @@ export class ExternalReferences {
 
     /**
      * Return index of reference to display
-     * return 0 if it was not found
+     * throws error if source name is not found
      * @param sourceName source name of reference
      */
     public getIndexOfReference(sourceName : string) : number {
         if(this._externalReferencesIndex.get(sourceName)) {
             return this._externalReferencesIndex.get(sourceName);
         }
-        return 0;
+        throw new Error(`could not find source name "${sourceName}"`);
     }
 
     /**
@@ -57,18 +57,23 @@ export class ExternalReferences {
     public set externalReferences(references: any) {
         if (references){
             // Create externalReferences list
-            for (var i = 0; i < references.length; i++){
+            for (let i = 0; i < references.length; i++){
 
-                var externalRef : ExternalReference = new ExternalReference();
-
-                externalRef.url = references[i].url;
-
+                let description = ""
                 if(references[i].description) {
-                    externalRef.description = references[i].description;
+                    description = references[i].description;
+                }
+                let url = ""
+                if(references[i].url) {
+                    url = references[i].url;
+                }
+
+                let externalRef : ExternalReference = {
+                    url : url,
+                    description : description
                 }
 
                 this._externalReferences.set(references[i]['source_name'], externalRef);
-
             }
 
             // Sort references by description and update index map
@@ -85,42 +90,9 @@ export class ExternalReferences {
     }
 }
 
-export class ExternalReference {
-    /** url; force */
-    private _url: string;
-    /** description; remove references from descriptive field if true */
-    private _description: string;
-
-    /**
-     * Construct an external reference object
-     */
-    constructor() {};
-
-    /**
-     * Set url 
-     * @param url string of reference url
-     */
-    public set url(url:string) { this._url = url; };
-
-    /**
-     * Set description 
-     * @param description string of reference description
-     */
-    public set description(description:string) { this._description = description; };
-
-    /**
-     * return external references list
-     */
-    public get description() : string { 
-        if (this._description) {
-            return this._description; 
-        }
-        return ""
-    }
-
-    /**
-     * return url
-     */
-    public get url() : string { return this._url; }
-
+export interface ExternalReference {
+    /** url; url of reference */
+    url: string;
+    /** description; description of reference */
+    description: string;
 }
