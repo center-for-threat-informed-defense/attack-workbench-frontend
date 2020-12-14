dc7e083aeffa95b2c6fce1d19c4ab52a05380197
Isabel Tuson <isabel@mitre.org>
2020-12-14 11:16:50 -0500

    further collection-index-view improvements

--- app/src/app/views/stix/collection/collection-index/collection-index-view/collection-index-view.component.ts
+++ app/src/app/views/stix/collection/collection-index/collection-index-view/collection-index-view.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { CollectionIndex } from 'src/app/classes/collection-index';
 
 @Component({
   selector: 'app-collection-index-view',
   templateUrl: './collection-index-view.component.html',
-  styleUrls: ['./collection-index-view.component.scss']
+  styleUrls: ['./collection-index-view.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CollectionIndexViewComponent implements OnInit {
+    @Input() index: CollectionIndex;
+    @Input() show_title: boolean = false; //if true,
+    @Input() config: CollectionIndexViewConfig;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+}
+export interface CollectionIndexViewConfig {
+    // the index to show
+    index: CollectionIndex;
+    // default false. If true, show the collection title in the component
+    show_title: boolean;
+    // default true. If false, hides subscribe actions from the component
+    show_actions: boolean;
 }
