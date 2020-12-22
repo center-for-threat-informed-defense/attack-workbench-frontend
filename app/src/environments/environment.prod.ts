d492d884580c38107e61e0dc541a3814cc9dfa14
Isabel Tuson <isabel@mitre.org>
2020-12-22 09:36:21 -0500

    update environments for #42

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    // configuration for the ATT&CK Workbench REST API
+    // https://github.com/center-for-threat-informed-defense/attack-workbench-rest-api
+    rest_api_url: "http://localhost",
+    rest_api_port: "3000",
+    // configuration for the ATT&CK Workbench Collection Manager
+    // https://github.com/center-for-threat-informed-defense/attack-workbench-collection-manager
+    collection_manager_url: "http://localhost",
+    collection_manager_port: "3001"
 };
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    // configuration for the ATT&CK Workbench REST API
+    // https://github.com/center-for-threat-informed-defense/attack-workbench-rest-api
+    rest_api_url: "http://localhost",
+    rest_api_port: "3000",
+    // configuration for the ATT&CK Workbench Collection Manager
+    // https://github.com/center-for-threat-informed-defense/attack-workbench-collection-manager
+    collection_manager_url: "http://localhost",
+    collection_manager_port: "3001"
 };
 
 /*
