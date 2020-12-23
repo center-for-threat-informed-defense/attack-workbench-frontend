6c7dc70183d5c8e1f8aea516791ea9c9c6ae8d71
Isabel Tuson <isabel@mitre.org>
2020-12-23 07:42:14 -0500

    initial collection index import UI

--- app/src/app/views/stix/collection/collection-index/collection-index-import/collection-index-import.component.ts
+++ app/src/app/views/stix/collection/collection-index/collection-index-import/collection-index-import.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatStepper } from '@angular/material/stepper';
 import { CollectionIndex } from 'src/app/classes/collection-index';
+import { CollectionManagerConnectorService } from 'src/app/services/connectors/collection-manager/collection-manager-connector.service';
 
 @Component({
   selector: 'app-collection-index-import',
@@ -10,13 +12,14 @@ import { CollectionIndex } from 'src/app/classes/collection-index';
   encapsulation: ViewEncapsulation.None
 })
 export class CollectionIndexImportComponent implements OnInit {
+    @ViewChild(MatStepper) public stepper: MatStepper;
 
-    constructor(public dialogRef: MatDialogRef<CollectionIndexImportComponent>) { }
+    constructor(public dialogRef: MatDialogRef<CollectionIndexImportComponent>, private collectionManagerConnector: CollectionManagerConnectorService) { }
 
     ngOnInit(): void {
     }
 
-    public url: string = "";
+    public url: string = "localhost:8082/collection-index.json";
 
     public index: CollectionIndex = null;
 
@@ -24,7 +27,13 @@ export class CollectionIndexImportComponent implements OnInit {
      * download the collection index at this.url and move to the next step in the stepper
      */
     public download(): void {
+        console.log("trigger download");
         // TODO interact with collection manager to trigger download process
+        this.collectionManagerConnector.getRemoteIndex(this.url).subscribe((index) => {
+            console.log("done");
+            this.index = index;
+            this.stepper.next();
+        })
     }
 
 }
