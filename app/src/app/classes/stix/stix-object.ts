976e415da5aae3dbdf3971fc784d25986b908893
Isabel Tuson <isabel@mitre.org>
2020-12-29 10:17:59 -0500

    update REST API connector with get functions for all current STIX types

--- app/src/app/classes/stix/stix-object.ts
+++ app/src/app/classes/stix/stix-object.ts
@@ -2,11 +2,12 @@ import { Relationship } from './relationship';
 import { VersionNumber } from '../version-number';
 import { ExternalReferences } from '../external-references';
 import { v4 as uuid } from 'uuid';
+import { Serializable } from '../serializable';
 
-export abstract class StixObject {
-    public readonly stixID: string; // STIX ID
-    public readonly type: string;   // STIX type
-    public readonly attackType: string; // ATT&CK type
+export abstract class StixObject extends Serializable {
+    public stixID: string; // STIX ID
+    public type: string;   // STIX type
+    public attackType: string; // ATT&CK type
     
 
     public get routes(): any[] { // route to view the object
@@ -23,8 +24,8 @@ export abstract class StixObject {
         ]
     } 
 
-    public readonly created: Date;  // object created date
-    public readonly modified: Date; // object modified date
+    public created: Date;  // object created date
+    public modified: Date; // object modified date
     public version: VersionNumber;  // version number of the object
     public external_references: ExternalReferences;
 
@@ -36,18 +37,9 @@ export abstract class StixObject {
      * @param sdo the STIX domain object to initialize data from
      */
     constructor(sdo?: any, type?: string) {
+        super();
         if (sdo) {
-            // initialize common fields from SDO
-            this.stixID = sdo.id;
-            this.type = sdo.type;
-            
-            this.created = new Date(sdo.created);
-            this.modified = new Date(sdo.modified);
-            this.version = sdo.x_mitre_version? new VersionNumber(sdo.x_mitre_version) : new VersionNumber("0.1");
-            this.external_references = new ExternalReferences(sdo.external_references);
-
-            if ("x_mitre_deprecated" in sdo) this.deprecated = sdo.x_mitre_deprecated;
-            if ("x_mitre_revoked" in sdo) this.revoked = sdo.x_mitre_revoked;
+            this.deserialize(sdo);
         } else {
             // create new SDO
             this.stixID = type + "--" + uuid();
@@ -68,6 +60,44 @@ export abstract class StixObject {
             "x-mitre-tactic": "tactic",
             "relationship": "relationship"
         }[this.type]
+    }
+
+    /**
+     * Transform the current object into a raw object for sending to the back-end, stripping any unnecessary fields
+     * @abstract
+     * @returns {*} the raw object to send
+     */
+    public serialize(): any {
+        return {
+            "type": this.type,
+            "id": this.stixID,
+            "created": this.created,
+            "modified": this.modified,
+            "version": this.version.toString(),
+            "external_references": this.external_references.serialize(),
+            "x_mitre_deprecated": this.deprecated,
+            "revoked": this.revoked
+        }
+    }
+
+    /**
+     * Parse the object from the record returned from the back-end
+     * @abstract
+     * @param {*} raw the raw object to parse
+     */
+    public deserialize(raw: any) {
+        let sdo = raw;
+        // initialize common fields from SDO
+        this.stixID = sdo.id;
+        this.type = sdo.type;
+        
+        this.created = new Date(sdo.created);
+        this.modified = new Date(sdo.modified);
+        this.version = sdo.x_mitre_version? new VersionNumber(sdo.x_mitre_version) : new VersionNumber("0.1");
+        this.external_references = new ExternalReferences(sdo.external_references);
+
+        if ("x_mitre_deprecated" in sdo) this.deprecated = sdo.x_mitre_deprecated;
+        if ("revoked" in sdo) this.revoked = sdo.x_mitre_revoked;
     }
 
     /**
