96210fa7476d96e3842d22a44a959621a2da71a6
Isabel Tuson <isabel@mitre.org>
2021-01-13 15:15:36 -0500

    basic collection preview implemented; TODO refactor into reusable component

--- app/src/app/classes/stix/collection.ts
+++ app/src/app/classes/stix/collection.ts
@@ -1,5 +1,38 @@
 import { StixObject } from './stix-object';
 
+/**
+ * 
+ * 
+ */
+
+/**
+ *auto-generated changelog/report about an import
+ * each sub-property is a list of objects corresponding to objects in the import
+ * @template T the type to record, typically a string for STIX IDs or a StixObject if the objects are being stratified directly
+ */
+export class CollectionImportCategories<T> {
+    additions: T[] = []; //new objects that didn't exist locally prior to the import
+    changes: T[] = []; //changes to objects that already existed locally
+    minor_changes: T[] = []; //objects which are changed without version number increments
+    deprecations: T[] = []; //objects that are now deprecated but weren't before
+    revocations: T[] = []; //objects which are now revoked but weren't before
+    supersedes_collection_changes: T[] = []; //objects which have a conflict where user edits supersede the collection object-version
+    supersedes_user_edits: T[] = []; //objects which have a conflict where they overwrite user changes
+    
+    /**
+     * get the number of objects recorded in the categories
+     */
+    public get object_count() { 
+        return this.additions.length +
+               this.changes.length +
+               this.deprecations.length +
+               this.minor_changes.length +
+               this.revocations.length +
+               this.supersedes_collection_changes.length +
+               this.supersedes_user_edits.length;
+    }
+}
+
 export class VersionReference {
     public object_ref: string;
     public object_modified: Date;
@@ -8,6 +41,7 @@ export class VersionReference {
         this.object_modified = new Date(raw.object_modified);
     }
 }   
+
 export class Collection extends StixObject {
     public name: string;
     public description: string;
@@ -15,15 +49,7 @@ export class Collection extends StixObject {
     public imported: Date;
      // auto-generated changelog/report about the import
     //  each sub-property is a list of STIX IDs corresponding to objects in the import
-    public import_categories: {
-        additions: string[], //new objects that didn't exist locally prior to the import
-        changes: string[], //changes to objects that already existed locally
-        deprecations: string[], //objects that are now deprecated but weren't before
-        minor_changes: string[], //objects which are changed without version number increments
-        revocations: string[], //objects which are now revoked but weren't before
-        supersedes_collection_changes: string[], //objects which have a conflict where user edits supersede the collection object-version
-        supersedes_user_edits: string[] //objects which have a conflict where they overwrite user changes
-    }
+    public import_categories: CollectionImportCategories<string>;
 
     constructor(sdo?: any) {
         super(sdo, "x-mitre-collection");
@@ -43,5 +69,11 @@ export class Collection extends StixObject {
         if ("x_mitre_contents" in raw.stix) this.contents = raw.stix.x_mitre_contents.map(vr => new VersionReference(vr))
         this.imported = new Date(raw.workspace.imported);
         this.import_categories = raw.workspace.import_categories;
+        // randomly assign objects to different categories
+        let categories = Object.keys(this.import_categories);
+        for (let vr of this.contents) {
+            let category = categories[ categories.length * Math.random() << 0];
+            this.import_categories[category].push(vr.object_ref)
+        }
     }
 }
