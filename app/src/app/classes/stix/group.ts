a7de7f83b3cb6de83912fba12afecfcf3875c8be
Isabel Tuson <isabel@mitre.org>
2021-01-19 15:07:19 -0500

    fix contributors, remove sorting

--- app/src/app/classes/stix/group.ts
+++ app/src/app/classes/stix/group.ts
@@ -6,7 +6,7 @@ export class Group extends StixObject {
     public description: string = "";
     public aliases: string[] = [];
     public attackID: string = "";
-    public contributors: string = "";
+    public contributors: string[] = [];
 
     constructor(sdo?: any) {
         super(sdo.stix, "intrusion-set");
@@ -15,7 +15,7 @@ export class Group extends StixObject {
             this.description = sdo.stix.description;
             this.aliases = sdo.stix.aliases ? sdo.stix.aliases : [];
             if (sdo.stix.external_references.length > 0) this.attackID = sdo.stix.external_references[0].external_id;
-            this.contributors = sdo.stix.x_mitre_contributors;
+            if (sdo.stix.x_mitre_contributors) this.contributors = sdo.stix.x_mitre_contributors;
         }
     }
     public deserialize(raw: any) {}
