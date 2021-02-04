e6687cadf1280cc8ea91c9705177aaf65e4b0b6d
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2021-02-04 11:48:18 -0500

    deserialize a relationship's source and target IDs

--- app/src/app/classes/stix/relationship.ts
+++ app/src/app/classes/stix/relationship.ts
@@ -4,9 +4,11 @@ export class Relationship extends StixObject {
 
     public readonly source_ref: string;
     public source_name: string = "[unknown object]";
+    public source_ID: string;
 
     public readonly target_ref: string;
     public target_name: string = "[unknown object]";
+    public target_ID: string;
     
     public readonly relationship_type: string;
     public description: string;
@@ -52,8 +54,31 @@ export class Relationship extends StixObject {
             } else this.description = "";
 
         }
-        if ("source_object" in raw) { this.source_name = raw.source_object.stix.name; }
-        if ("target_object" in raw) { this.target_name = raw.target_object.stix.name; }
+
+        if ("source_object" in raw) {
+            this.source_name = raw.source_object.stix.name;
+            
+            let src_sdo = raw.source_object.stix;
+            if ("external_references" in src_sdo) {
+                if (src_sdo.external_references.length > 0) {
+                    if (typeof(src_sdo.external_references[0].external_id) === "string") this.source_ID = src_sdo.external_references[0].external_id;
+                    else console.error("TypeError: attackID field is not a string:", src_sdo.external_references[0].external_id, "(", typeof(src_sdo.external_references[0].external_id), ")");
+                }
+                else console.error("ObjectError: external references is empty");
+            } else this.source_ID = "";
+        }
+        if ("target_object" in raw) {
+            this.target_name = raw.target_object.stix.name;
+
+            let tgt_sdo = raw.target_object.stix;
+            if ("external_references" in tgt_sdo) {
+                if (tgt_sdo.external_references.length > 0) {
+                    if (typeof(tgt_sdo.external_references[0].external_id) === "string") this.target_ID = tgt_sdo.external_references[0].external_id;
+                    else console.error("TypeError: attackID field is not a string:", tgt_sdo.external_references[0].external_id, "(", typeof(tgt_sdo.external_references[0].external_id), ")");
+                }
+                else console.error("ObjectError: external references is empty");
+            } else this.target_ID = "";
+        }
     }
     
     /**
