2b13d9acc50ccb16e805a906fcee0b13bdf7a3fd
Isabel Tuson <isabel@mitre.org>
2021-02-04 13:25:07 -0500

    move editing state functions to editor.service

--- app/src/app/services/editor/editor.service.ts
+++ app/src/app/services/editor/editor.service.ts
@@ -1,23 +1,45 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { SaveDialogComponent } from 'src/app/components/save-dialog/save-dialog.component';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditorService {
-    constructor(private dialog: MatDialog) { }
+
+    public editable: boolean = false;
+    public editing: boolean = false;
     public onSave = new EventEmitter();
     
-    // public save() {
-    //     let prompt = this.dialog.open(SaveDialogComponent, {
-    //         maxWidth: "35em"
-    //     });
-    //     let subscription = prompt.afterClosed().subscribe({
-    //         next: (result) => {
-    //             console.log(result);
-    //         },
-    //         complete: () => { subscription.unsubscribe(); } //prevent memory leaks
-    //     })
-    // }
+    constructor(private router: Router, private route: ActivatedRoute) {
+        this.router.events.subscribe(event => { 
+            if (event instanceof NavigationEnd) { 
+                let editable = this.getEditableFromRoute(this.router.routerState, this.router.routerState.root);
+                this.editable = editable.length > 0 && editable.every(x=>x);
+            }
+        })
+        this.route.queryParams.subscribe(params => {
+            this.editing = params["editing"];
+        });
+    }
+
+    public startEditing() {
+        if (this.editable)  this.router.navigate([], {queryParams: { editing: true }})
+    }
+
+    public stopEditing() {
+        this.router.navigate([], {queryParams: {}})
+    }
+
+     //https://stackoverflow.com/questions/38644314/changing-the-page-title-using-the-angular-2-new-router/38652281#38652281
+     private getEditableFromRoute(state, parent) { 
+        let data = [];
+        if (parent && parent.snapshot.data && parent.snapshot.data.editable) {
+            data.push(parent.snapshot.data.editable);
+        }
+    
+        if(state && parent) {
+            data.push(... this.getEditableFromRoute(state, state.firstChild(parent)));
+        }
+        return data;
+    }
 }
