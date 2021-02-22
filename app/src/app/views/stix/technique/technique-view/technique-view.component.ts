65975a2ae73043e69a30b26e69cabc7e6858a6c5
idavila <idavila>
2021-02-22 13:21:06 -0500

    Loading objects when navigation ends

--- app/src/app/views/stix/technique/technique-view/technique-view.component.ts
+++ app/src/app/views/stix/technique/technique-view/technique-view.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Technique } from 'src/app/classes/stix/technique';
 import { StixViewPage } from '../../stix-view-page';
-import { Relationship } from 'src/app/classes/stix/relationship';
-import { Tactic } from 'src/app/classes/stix/tactic';
 
 @Component({
   selector: 'app-technique-view',
@@ -17,30 +15,14 @@ export class TechniqueViewComponent extends StixViewPage implements OnInit {
 
     public get technique(): Technique { return this.config.object as Technique; }
 
-    constructor(private router: Router, private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute) {
         super();
     }
 
     ngOnInit() {
-        this.router.events.subscribe(event => { 
-            if (event instanceof NavigationEnd) { 
-                // trick the Router into believing it's last link wasn't previously loaded
-                this.router.navigated = true;
-                // if you need to scroll back to top, here is the right place
-                window.scrollTo(0, 0);
-            }
-        })
-
         this.route.queryParams.subscribe(params => {
             this.editing = params["editing"];
         });
-    
-        console.log(this.technique);
-
-    }
-
-    ngOnDestroy() {
-        // this.router.events.unsubscribe();
     }
 
 }
