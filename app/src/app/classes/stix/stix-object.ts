da4fcd694b7046e17e7f577f6be6a74ef128ab3b
Isabel Tuson <isabel@mitre.org>
2021-03-03 12:05:02 -0500

    prevent technique<->sub-technique conversions if subtechnique-of relationships exist with it

--- app/src/app/classes/stix/stix-object.ts
+++ app/src/app/classes/stix/stix-object.ts
@@ -288,7 +288,7 @@ export abstract class StixObject extends Serializable {
                             }
                             let attackIDValid = 
                                 this.type == "attack-pattern"? 
-                                    this["is_subtechnique"] ? /^T\d{4}(.\d{3})?$/.test(this.attackID) : 
+                                    this["is_subtechnique"] ? /^T\d{4}(.\d{3})$/.test(this.attackID) : 
                                                               /^T\d{4}$/.test(this.attackID) :
                                 this.type == "x-mitre-tactic"? /^TA\d{4}$/.test(this.attackID) :
                                 this.type == "malware" || this.type == "tool"? /^S\d{4}$/.test(this.attackID) :
