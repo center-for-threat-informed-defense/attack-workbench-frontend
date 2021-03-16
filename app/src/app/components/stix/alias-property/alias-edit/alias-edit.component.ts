45207b8f8ef87f4f7b2890427b1da7b01b335d27
Isabel Tuson <isabel@mitre.org>
2021-03-16 12:37:21 -0400

    can now delete aliases

--- app/src/app/components/stix/alias-property/alias-edit/alias-edit.component.ts
+++ app/src/app/components/stix/alias-property/alias-edit/alias-edit.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { StixObject } from 'src/app/classes/stix/stix-object';
+import { AliasPropertyConfig } from '../alias-property.component';
 
 @Component({
   selector: 'app-alias-edit',
   templateUrl: './alias-edit.component.html',
-  styleUrls: ['./alias-edit.component.scss']
+  styleUrls: ['./alias-edit.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AliasEditComponent implements OnInit {
+    @Input() public config: AliasPropertyConfig;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    public editAlias(aliasName: string) {
+
+    }
+
+    public removeAlias(aliasName: string) {
+        console.log("removing alias", aliasName);
+        let obj = this.config.object as StixObject;
+        // remove from alias field
+        obj[this.config.field] = obj[this.config.field].filter((x) => x != aliasName)
+        // remove external reference citation
+        let references = obj.external_references.serialize();
+        references = references.filter((ref) => {
+            if (ref.source_name == aliasName) { console.log("removing ref", ref); return false };
+            return true;
+        });
+        obj.external_references.deserialize(references);
+        console.log(obj.external_references);
+    }
 
 }
