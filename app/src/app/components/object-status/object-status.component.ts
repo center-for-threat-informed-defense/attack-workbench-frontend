5bd3568d77bfc5ce76d0a7e215b36f972c69174e
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2021-04-01 11:25:25 -0400

    handle workflow state change

--- app/src/app/components/object-status/object-status.component.ts
+++ app/src/app/components/object-status/object-status.component.ts
@@ -15,11 +15,17 @@ export class ObjectStatusComponent implements OnInit {
     public statusControl: FormControl;
     public workflows: string[] = ["work-in-progress", "awaiting-review", "reviewed"];
 
+    public object: StixObject;
+    public revoked: boolean = false;
+    public deprecated: boolean = false;
+    public workflow: string;
+
     constructor(private editorService: EditorService, private restAPIService: RestApiConnectorService) { }
 
     ngOnInit(): void {
         this.statusControl = new FormControl();
         
+        // retrieve the object
         let data$: any;
         if (this.editorService.type == "software") data$ = this.restAPIService.getSoftware(this.editorService.stixId);
         else if (this.editorService.type == "group") data$ = this.restAPIService.getGroup(this.editorService.stixId);
@@ -29,12 +35,36 @@ export class ObjectStatusComponent implements OnInit {
         else if (this.editorService.type == "technique") data$ = this.restAPIService.getTechnique(this.editorService.stixId);
         let subscription = data$.subscribe({
             next: (data) => {
-                let object = data as StixObject;
-                if (object.workflow && object.workflow.state) {
-                    this.statusControl.setValue(object.workflow.state)
+                this.object = data[0];
+                if (this.object.workflow && this.object.workflow.state) {
+                    this.statusControl.setValue(this.object.workflow.state);
+                    this.workflow = this.object.workflow.state;
                 }
+                this.revoked = this.object.revoked;
+                this.deprecated = this.object.deprecated;
             },
             complete: () => { subscription.unsubscribe() }
         });
+    }
+
+    /**
+     * Handle workflow state change
+     * @param event workflow state selection
+     */
+    public workflowChange(event) {
+        if (event.isUserInput) {
+            this.object.workflow = {state: event.source.value};
+            this.object.save(this.restAPIService);
+        }
+    }
+
+    /**
+     * 
+     * @param event 
+     */
+    public revoke(event) {
+    }
+
+    public deprecate(event) {
     }
 }
