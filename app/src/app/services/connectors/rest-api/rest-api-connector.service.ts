378111da1198cadfcc311c3df19d75efa3501ecf
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2021-04-05 09:48:08 -0400

    bug fix for unmatching revoked/deprecated variables in rest api service

--- app/src/app/services/connectors/rest-api/rest-api-connector.service.ts
+++ app/src/app/services/connectors/rest-api/rest-api-connector.service.ts
@@ -107,7 +107,7 @@ export class RestApiConnectorService extends ApiConnector {
     private getStixObjectsFactory<T extends StixObject>(attackType: AttackType) {
         let attackClass = attackTypeToClass[attackType];
         let plural = attackTypeToPlural[attackType]
-        return function<P extends T>(options?: { limit?: number, offset?: number, state?: string, revoked?: boolean, deprecated?: boolean, versions?: "all" | "latest", excludeIDs?: string[], search?: string }): Observable<Paginated<StixObject>> {
+        return function<P extends T>(options?: { limit?: number, offset?: number, state?: string, includeRevoked?: boolean, includeDeprecated?: boolean, versions?: "all" | "latest", excludeIDs?: string[], search?: string }): Observable<Paginated<StixObject>> {
             // parse params into query string
             let query = new HttpParams();
             // pagination
@@ -116,8 +116,8 @@ export class RestApiConnectorService extends ApiConnector {
             if (options && (options.limit || options.offset)) query = query.set("includePagination", "true");
             // other properties
             if (options && options.state) query = query.set("state", options.state);
-            if (options && options.revoked) query = query.set("revoked", options.revoked ? "true" : "false");
-            if (options && options.revoked) query = query.set("deprecated", options.deprecated ? "true" : "false");
+            if (options && options.includeRevoked) query = query.set("includeRevoked", options.includeRevoked ? "true" : "false");
+            if (options && options.includeDeprecated) query = query.set("includeDeprecated", options.includeDeprecated ? "true" : "false");
             if (options && options.versions) query = query.set("versions", options.versions);
             if (options && options.search) query = query.set("search", encodeURIComponent(options.search));
             // perform the request
