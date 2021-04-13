7e2fcde4f8cbd524b30150c2d35773cffb292c1b
Isabel Tuson <isabel@mitre.org>
2021-04-13 16:08:32 -0400

    add basic integrations and org-identity page

---
+++ app/src/app/views/admin-page/org-identity-page/org-identity-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-org-identity-page',
+  templateUrl: './org-identity-page.component.html',
+  styleUrls: ['./org-identity-page.component.scss']
+})
+export class OrgIdentityPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
