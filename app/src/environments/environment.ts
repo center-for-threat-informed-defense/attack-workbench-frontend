36c7cf0e933a8fbfaaeef1d679af9929efcc200c
Isabel Tuson <isabel@mitre.org>
2021-04-27 13:02:09 -0400

    add recommended indexes feature

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -4,6 +4,12 @@
 
 export const environment = {
     production: false,
+    recommended_indexes: [ //recommended collection indexes shown to user if they have no indexes loaded
+        {
+            "name": "MITRE ATT&CK", //index name
+            "url": "http://localhost:8082/collection-index.json" //index url
+        }
+    ],
     integrations: {
         rest_api: {
             // configuration for the ATT&CK Workbench REST API
