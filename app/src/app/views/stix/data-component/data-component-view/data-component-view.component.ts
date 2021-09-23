a17b310725a52030c6b3e72a7b130493042fdc9a
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2021-09-23 15:34:28 -0400

    retrieve 'parent' data source

--- app/src/app/views/stix/data-component/data-component-view/data-component-view.component.ts
+++ app/src/app/views/stix/data-component/data-component-view/data-component-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { DataComponent } from 'src/app/classes/stix/data-component';
+import { DataSource } from 'src/app/classes/stix/data-source';
 import { StixObject } from 'src/app/classes/stix/stix-object';
 import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
 import { StixViewPage } from '../../stix-view-page';
@@ -11,13 +12,22 @@ import { StixViewPage } from '../../stix-view-page';
 })
 export class DataComponentViewComponent extends StixViewPage implements OnInit {
     @Output() onClickRelationship = new EventEmitter();
-    
+
     public get data_component() { return this.config.object as DataComponent; }
+    public data_source: DataSource = null;
 
     constructor(private restAPIConnectorService: RestApiConnectorService) { super(); }
 
     ngOnInit(): void {
-        // TODO retrieve parent data source
+        // retrieve parent data source
+        let objects$ = this.restAPIConnectorService.getDataSource(this.data_component.data_source_ref);
+        let subscription = objects$.subscribe({
+            next: (result) => {
+                let objects = result as DataSource[];
+                this.data_source = objects[0];
+            },
+            complete: () => { subscription.unsubscribe() }
+        });
     }
 
     public viewRelationship(object: StixObject): void {
