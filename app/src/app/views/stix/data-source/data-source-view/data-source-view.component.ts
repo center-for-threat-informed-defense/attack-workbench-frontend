67c43ebad315cbd05a4666de325987cc1bf54d57
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2021-09-27 15:33:03 -0400

    create a data component

--- app/src/app/views/stix/data-source/data-source-view/data-source-view.component.ts
+++ app/src/app/views/stix/data-source/data-source-view/data-source-view.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DataSource } from 'src/app/classes/stix/data-source';
+import { DataComponent } from 'src/app/classes/stix/data-component';
+import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
 import { StixViewPage } from '../../stix-view-page';
-import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { StixDialogComponent } from '../../stix-dialog/stix-dialog.component';
 
 @Component({
     selector: 'app-data-source-view',
@@ -10,8 +13,46 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DataSourceViewComponent extends StixViewPage implements OnInit {
     public get data_source(): DataSource { return this.config.object as DataSource; }
+    public data_components: DataComponent[] = [];
+    public loading = false;
 
-    constructor(private route: ActivatedRoute) { super(); }
+    constructor(public dialog: MatDialog, private restAPIConnectorService: RestApiConnectorService) { super(); }
 
-    ngOnInit(): void { }
+    ngOnInit(): void {
+        this.getDataComponents();
+    }
+
+    public getDataComponents(): void {
+        this.loading = true;
+        let data$ = this.restAPIConnectorService.getAllDataComponents();
+        let sub = data$.subscribe({
+            next: (results) => {
+                let objects = results.data as DataComponent[];
+                this.data_components = objects.filter(obj => obj.data_source_ref == this.data_source.stixID)
+                this.loading = false;
+            }, 
+            complete: () => {sub.unsubscribe();}
+        })
+    }
+
+    public createDataComponent(): void {
+        let data_component = new DataComponent();
+        data_component.data_source_ref = this.data_source.stixID;
+        let prompt = this.dialog.open(StixDialogComponent, {
+            data: {
+                object: data_component,
+                editable: true,
+                mode: "edit",
+                sidebarControl: "events"
+            }
+        });
+        let subscription = prompt.afterClosed().subscribe({
+            next: (result) => {
+                if (result) {
+                    if (prompt.componentInstance.dirty) this.getDataComponents(); //re-fetch values since an edit occurred
+                }
+            },
+            complete: () => { subscription.unsubscribe(); }
+        });
+    }
 }
