565e3f11265a2dcfab4188dd08cb6c43249bbd2c
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2021-10-18 15:10:51 -0400

    increased error reporting & review updates

--- app/src/app/views/stix/collection/collection-import/collection-import-error/collection-import-error.component.ts
+++ app/src/app/views/stix/collection/collection-import/collection-import-error/collection-import-error.component.ts
@@ -8,8 +8,24 @@ import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angu
 })
 export class CollectionImportErrorComponent {
     @Input() error: any;
-    @Output() onForceImport = new EventEmitter<boolean>();
+    @Output() onForceImport = new EventEmitter();
+    @Output() onCancel = new EventEmitter();
 
+    public get hasWarnings(): boolean {
+        return this.duplicateCollection || this.duplicateObjects > 0 || this.objSpecVersionViolations > 0 || this.objSpecViolations > 0;
+    }
+    public get hasErrors(): boolean {
+        return this.noCollection || this.multipleCollections || this.badlyFormatted;
+    }
+
+    // can be overridden
     public get duplicateCollection(): boolean { return this.error.bundleErrors.duplicateCollection; }
-    public get objectSpecVersionViolation(): number { return this.error.objectErrors.summary.invalidAttackSpecVersionCount; }
+    public get duplicateObjects(): number { return this.error.objectErrors.summary.duplicateObjectInBundleCount; }
+    public get objSpecVersionViolations(): number { return this.error.objectErrors.summary.invalidAttackSpecVersionCount; }
+    public get objSpecViolations(): number { return this.error.objectErrors.errors.length - this.objSpecVersionViolations - this.duplicateObjects; }
+
+    // cannot be overridden
+    public get noCollection(): boolean { return this.error.bundleErrors.noCollection; }
+    public get multipleCollections(): boolean { return this.error.bundleErrors.moreThanOneCollection; }
+    public get badlyFormatted(): boolean { return this.error.bundleErrors.badlyFormattedCollection; }
 }
