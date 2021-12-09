cb0d689a51c01aebb2120e7e190c3c782616a404
Anna Lin <alin@mitre.org>
2021-12-08 23:02:19 -0500

    Added skeleton of user accounts table, took styling from stix-list table

--- app/src/app/views/admin-page/user-accounts-page/user-accounts-page.component.ts
+++ app/src/app/views/admin-page/user-accounts-page/user-accounts-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserAccount } from 'src/app/classes/authn/user-account';
 import { Paginated, RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
@@ -6,12 +6,19 @@ import { Paginated, RestApiConnectorService } from 'src/app/services/connectors/
 @Component({
     selector: 'app-user-accounts-page',
     templateUrl: './user-accounts-page.component.html',
-    styleUrls: ['./user-accounts-page.component.scss']
+    styleUrls: ['./user-accounts-page.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class UserAccountsPageComponent implements OnInit {
     public userAccounts$: Observable<Paginated<UserAccount>>;
+    public columnsToDisplay: string[];
+    public searchQuery = '';
+    public filterOptions: Role;
+    public filter: string[] = [];
 
-    constructor(private restAPIConnector: RestApiConnectorService) { }
+    constructor(private restAPIConnector: RestApiConnectorService) {
+      this.columnsToDisplay = ['username', 'email', 'status', 'role'];
+    }
 
     ngOnInit(): void {
         this.userAccounts$ = this.restAPIConnector.getAllUserAccounts();
