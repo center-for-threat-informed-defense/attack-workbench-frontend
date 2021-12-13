91cc2cb6bc3f5ea1726980249ce2adc6303eabc6
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2021-12-13 14:59:32 -0500

    fix for issues retrieving collection bundles & indexes by URL

--- app/src/app/services/helpers/auth.interceptor.ts
+++ app/src/app/services/helpers/auth.interceptor.ts
@@ -16,10 +16,12 @@ export class AuthInterceptor implements HttpInterceptor {
 
     // intercept outgoing requests to include headers & credentials
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const excludeCredentials = request.headers.get('SkipInterceptor');
+        const newHeaders = excludeCredentials ? request.headers.delete('SkipInterceptor') : this.headers;
         request = request.clone({
-            headers: this.headers,
-            withCredentials: true
-        });
+            headers: newHeaders,
+            withCredentials: excludeCredentials ? false : true
+        })
         return next.handle(request);
     }
 }
