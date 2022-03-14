dc91ceb3ada4fd2fb5bfa3b9183031386265bdea
Anna Lin <alin@mitre.org>
2022-03-14 18:22:15 -0400

    Set prefix to be uneditable in attackid-edit.component

--- app/src/app/components/stix/attackid-property/attackid-edit/attackid-edit.component.ts
+++ app/src/app/components/stix/attackid-property/attackid-edit/attackid-edit.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { AttackIDPropertyConfig } from '../attackid-property.component';
 
 @Component({
   selector: 'app-attackid-edit',
   templateUrl: './attackid-edit.component.html',
-  styleUrls: ['./attackid-edit.component.scss']
+  styleUrls: ['./attackid-edit.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AttackIDEditComponent implements OnInit {
   @Input() public config: AttackIDPropertyConfig;
+  public prefix: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getPrefix(attackID) {
+    if (attackID.split('-').length > 1) {
+      this.prefix = attackID.split('-')[0]
+      return this.prefix;
+    }
+    return ''
   }
 
+  getEditable(attackID) {
+    if (attackID.split('-').length > 1)
+      return attackID.split('-')[1]
+    return attackID
+  }
 }
