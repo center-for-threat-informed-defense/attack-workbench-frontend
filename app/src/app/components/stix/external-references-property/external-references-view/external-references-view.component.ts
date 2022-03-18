611a198ef0bb70cbccb4a1e48873acc56457ae0a
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2022-03-18 14:53:07 -0400

    reload references when done editing

--- app/src/app/components/stix/external-references-property/external-references-view/external-references-view.component.ts
+++ app/src/app/components/stix/external-references-property/external-references-view/external-references-view.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ExternalReference } from 'src/app/classes/external-references';
+import { EditorService } from 'src/app/services/editor/editor.service';
 import { ExternalReferencesPropertyConfig } from '../external-references-property.component';
 
 @Component({
@@ -8,13 +10,26 @@ import { ExternalReferencesPropertyConfig } from '../external-references-propert
     styleUrls: ['./external-references-view.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class ExternalReferencesViewComponent implements OnInit {
+export class ExternalReferencesViewComponent implements OnInit, OnDestroy {
     @Input() public config: ExternalReferencesPropertyConfig;
+    public onEditStopSubscription: Subscription;
     public referenceList: Array<[number, ExternalReference]> = [];
 
-    constructor() { }
+    constructor(private editorService: EditorService) {
+        this.onEditStopSubscription = this.editorService.onEditingStopped.subscribe({
+            next: () => { this.loadReferences(); } // reload references when done editing
+        })
+    }
 
     ngOnInit(): void {
+        this.loadReferences();
+    }
+
+    ngOnDestroy(): void {
+        this.onEditStopSubscription.unsubscribe();
+    }
+
+    public loadReferences(): void {
         this.referenceList = this.config.referencesField.list();
     }
 }
