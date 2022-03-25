93d229c8ed70d25db7c011572213fdb6887785dc
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2022-03-25 11:32:15 -0400

    remove unused event emitter

--- app/src/app/services/editor/editor.service.ts
+++ app/src/app/services/editor/editor.service.ts
@@ -13,7 +13,6 @@ export class EditorService {
     public editable: boolean = false;
     public editing: boolean = false;
     public onSave = new EventEmitter();
-    public onRelationshipSave = new EventEmitter();
     public onEditingStopped = new EventEmitter();
     public onReload = new EventEmitter();
     
