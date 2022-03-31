c89bce756d838c7e416a9fdfef3f18e1649409d3
Anna Lin <alin@mitre.org>
2022-03-31 11:12:43 -0400

    Fixed org settings field input validity check

--- app/src/app/views/admin-page/org-settings-page/org-settings-page.component.ts
+++ app/src/app/views/admin-page/org-settings-page/org-settings-page.component.ts
@@ -11,43 +11,53 @@ export class OrgSettingsPageComponent implements OnInit {
     public organizationIdentity: Identity;
     public organizationNamespace: Namespace;
     public idRegex = `^([A-Za-z])*$`;
-    public rangeRegex = `^([0-9]){0,4}$`;
+    public rangeRegex = `^([0-9]){1,4}$`;
+
+    public isNOU = (x) => x === undefined || x === null; // isNullOrUndefined
+
     public get isNamespaceInvalid(): boolean {
       const regid = new RegExp(this.idRegex);
       const regrange = new RegExp(this.rangeRegex);
-      return !regid.test(this.organizationNamespace.prefix) || (this.organizationNamespace.range_start && !regrange.test(this.organizationNamespace.range_start?.toString()));
+      return (this.organizationNamespace.prefix === '' && this.isNOU(this.organizationNamespace.range_start)) ||
+              (!regid.test(this.organizationNamespace.prefix)) ||
+              (!this.isNOU(this.organizationNamespace.range_start) && !regrange.test(this.organizationNamespace.range_start?.toString()));
     }
 
     constructor(private restAPIConnector: RestApiConnectorService) { }
 
     ngOnInit(): void {
-        let idSub = this.restAPIConnector.getOrganizationIdentity().subscribe({
+        const idSub = this.restAPIConnector.getOrganizationIdentity().subscribe({
             next: (identity) => this.organizationIdentity = identity,
             complete: () => idSub.unsubscribe()
         });
 
-        let namespaceSub = this.restAPIConnector.getOrganizationNamespace().subscribe({
-            next: (namespaceSettings) =>
-              this.organizationNamespace = {...namespaceSettings, range_start: namespaceSettings.range_start ? namespaceSettings.range_start.toString().padStart(4, '0') : undefined},
+        const namespaceSub = this.restAPIConnector.getOrganizationNamespace().subscribe({
+            next: (namespaceSettings) => {
+              this.organizationNamespace = {
+                ...namespaceSettings,
+                range_start: namespaceSettings.range_start ? namespaceSettings.range_start.toString().padStart(4, '0') : undefined
+              };
+            },
             complete: () => namespaceSub.unsubscribe()
-        })
+        });
 
     }
 
-    onBlur() {
-      if (!this.organizationNamespace.range_start) return;
-      this.organizationNamespace.range_start = this.organizationNamespace.range_start.toString().padStart(4, '0');
+    onBlur(): void {
+      if (!this.isNOU(this.organizationNamespace.range_start)) {
+        this.organizationNamespace.range_start = this.organizationNamespace.range_start.toString().padStart(4, '0');
+      }
     }
 
-    saveIdentity() {
-        let subscription = this.restAPIConnector.postIdentity(this.organizationIdentity).subscribe({
+    saveIdentity(): void {
+        const subscription = this.restAPIConnector.postIdentity(this.organizationIdentity).subscribe({
             next: (identity) => this.organizationIdentity = identity,
             complete: () => subscription.unsubscribe()
         });
     }
 
-    saveNamespace() {
-        let subscription = this.restAPIConnector.setOrganizationNamespace(this.organizationNamespace).subscribe({
+    saveNamespace(): void {
+        const subscription = this.restAPIConnector.setOrganizationNamespace(this.organizationNamespace).subscribe({
             next: (namespace) => this.organizationNamespace = namespace,
             complete: () => subscription.unsubscribe()
         });
