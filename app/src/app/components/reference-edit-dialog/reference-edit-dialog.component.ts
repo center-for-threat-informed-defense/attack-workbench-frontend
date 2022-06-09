c9706d0038f95e9c438a9f6a2f7ff32b1cd289f5
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2022-06-09 11:22:53 -0400

    add mat errors to source_name field and implement custom validators

--- app/src/app/components/reference-edit-dialog/reference-edit-dialog.component.ts
+++ app/src/app/components/reference-edit-dialog/reference-edit-dialog.component.ts
@@ -1,8 +1,9 @@
-import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, Inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl, ValidationErrors, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { forkJoin } from 'rxjs';
+import { forkJoin, Observable, of, Subscription, throwError } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
 import { ExternalReference } from 'src/app/classes/external-references';
 import { Relationship } from 'src/app/classes/stix/relationship';
 import { StixObject } from 'src/app/classes/stix/stix-object';
@@ -15,13 +16,17 @@ import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/re
   styleUrls: ['./reference-edit-dialog.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ReferenceEditDialogComponent implements OnInit {
+export class ReferenceEditDialogComponent implements OnInit, OnDestroy {
     public reference: ExternalReference;
     public is_new: boolean;
     public stage: number = 0;
     public patch_objects: StixObject[];
     public patch_relationships: Relationship[];
     public dirty: boolean;
+
+    public references$: ExternalReference[];
+    public source_control: FormControl;
+    public validationSubscription: Subscription;
 
     public months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     public citation: any = {};
@@ -49,12 +54,30 @@ export class ReferenceEditDialogComponent implements OnInit {
                 url: "",
                 description: ""
             }
+            this.source_control = new FormControl(null);
         }
     }
 
     ngOnInit(): void {
-        // intentionally left blank
-    } 
+        // retrieve all references
+        let referenceSubscription = this.restApiConnectorService.getAllReferences().subscribe({
+            next: (data) => {
+                this.references$ = data.data;
+            },
+            complete: () => referenceSubscription.unsubscribe()
+        })
+
+        // listen to source_name input changes
+        this.validationSubscription = this.source_control.valueChanges.subscribe(source_name => {
+            this.validate(this.source_control.value).subscribe({
+                error: (err) => { if (err) this.source_control.setErrors(err); }
+            })
+        })
+    }
+
+    ngOnDestroy(): void {
+        if (this.validationSubscription) this.validationSubscription.unsubscribe();
+    }
 
     public next() {
         if (this.is_new) {
@@ -168,6 +191,32 @@ export class ReferenceEditDialogComponent implements OnInit {
                 subscription.unsubscribe();
             }
         });
+    }
+
+    /**
+     * Validate reference source name
+     * @param source_name the source name input
+     * @returns 
+     */
+    public validate(source_name): Observable<ValidationErrors> {
+        this.source_control.setErrors(null); // clear previous validation
+        // required
+        if (!source_name) return throwError({required: true});
+
+        // cannot contain parenthesis
+        let regex = /(\(|\))/g;
+        if (regex.test(source_name)) return throwError({containsInvalidChar: true});
+
+        // uniqueness
+        if (this.references$.some(x => x.source_name == source_name)) return throwError({nonUnique: true});
+
+        return of();
+    }
+
+    /** Retrieve the validation error for display */
+    public getError(): string {
+        if (this.source_control.errors.containsInvalidChar) return 'source name cannot contain parenthesis';
+        if (this.source_control.errors.nonUnique) return 'source name is not unique';
     }
 
     public stopEditing(): void {
