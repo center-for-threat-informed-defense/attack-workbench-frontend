7263b1ebf23c2e06b151e7bf30061ddb15fab428
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2022-06-30 13:19:45 -0400

    campaign list page

--- app/src/app/views/stix/campaigns/campaign-list/campaign-list.component.ts
+++ app/src/app/views/stix/campaigns/campaign-list/campaign-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/connectors/authentication/authentication.service';
 
 @Component({
-  selector: 'app-campaign-list',
-  templateUrl: './campaign-list.component.html',
-  styleUrls: ['./campaign-list.component.scss']
+    selector: 'app-campaign-list',
+    templateUrl: './campaign-list.component.html',
+    styleUrls: ['./campaign-list.component.scss']
 })
 export class CampaignListComponent implements OnInit {
+    public get canEdit(): boolean { return this.authenticationService.canEdit(); }
 
-  constructor() { }
+    constructor(private authenticationService: AuthenticationService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        // intentionally left blank
+    }
 
 }
