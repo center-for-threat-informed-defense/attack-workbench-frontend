a6243000ea71cb263699579cb8516f0246aadb1a
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2022-07-18 14:36:02 -0400

    fix timestamp edit path

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -86,7 +86,7 @@ import { DescriptiveDiffComponent } from './components/stix/descriptive-property
 
 import { TimestampPropertyComponent } from "./components/stix/timestamp-property/timestamp-property.component";
 import { TimestampViewComponent } from "./components/stix/timestamp-property/timestamp-view/timestamp-view.component";
-import { TimestampEditComponent } from './components/stix/timestamp-property/timestamp-edit/timestamp-edit/timestamp-edit.component';
+import { TimestampEditComponent } from './components/stix/timestamp-property/timestamp-edit/timestamp-edit.component';
 import { TimestampDiffComponent } from "./components/stix/timestamp-property/timestamp-diff/timestamp-diff.component";
 
 import { StatementPropertyComponent } from "./components/stix/statement-property/statement-property.component";
