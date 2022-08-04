1c6c149598862a9138478d8f7030db366bdad1e3
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2022-08-04 12:12:56 -0400

    emulate month/year datepicker

--- app/src/app/components/stix/datepicker-property/datepicker-property.component.ts
+++ app/src/app/components/stix/datepicker-property/datepicker-property.component.ts
@@ -1,26 +1,62 @@
 import { Component, Input } from '@angular/core';
 import { StixObject } from 'src/app/classes/stix/stix-object';
+import { MatDatepicker } from '@angular/material/datepicker';
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { Moment } from 'moment';
 import * as moment from 'moment';
+
+// Date formatting from Angular documentation: emulating a year and month picker
+// https://v10.material.angular.io/components/datepicker/overview
+// https://momentjs.com/docs/#/displaying/format/
+
+export const DATE_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY', // date input format
+  },
+  display: {
+    dateInput: 'MM/YYYY', // format for displaying selected date
+    monthYearLabel: 'MMM YYYY', // datepicker display format
+    dateA11yLabel: 'LL', // format read by screen readers during day selection
+    monthYearA11yLabel: 'MMMM YYYY', // format read by screen readers during month selection
+  },
+};
 
 @Component({
     selector: 'app-datepicker-property',
     templateUrl: './datepicker-property.component.html',
-    styleUrls: ['./datepicker-property.component.scss']
+    styleUrls: ['./datepicker-property.component.scss'],
+    providers: [
+        // date format providers
+        {
+          provide: DateAdapter,
+          useClass: MomentDateAdapter,
+          deps: [ MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS ]
+        },
+        {
+            provide: MAT_DATE_FORMATS,
+            useValue: DATE_FORMATS
+        }
+      ],
 })
 
 export class DatepickerPropertyComponent {
     @Input() public config: DatepickerPropertyConfig;
 
-    public get date() {
-        let m = moment(this.config.object[this.config.field]);
-        return m.format(this.config.dateFormat ? this.config.dateFormat : 'MM/DD/YYYY');
+    public get formatHint() { return DATE_FORMATS.parse.dateInput; }
+    public get date() { // get date in view display format (i.e. January 2022)
+        if (this.config.object[this.config.field]) {
+            return moment(this.config.object[this.config.field]).format('MMMM YYYY');
+        }
+        return null;
     }
 
     constructor() {
         // intentionally left blank
     }
 
-    public setDate(event: moment.Moment, datepicker: any) {
+    // event handler for datepicker month selection
+    public monthSelected(event: Moment, datepicker: MatDatepicker<Moment>) {
         this.config.object[this.config.field] = event;
         datepicker.close();
     }
@@ -38,6 +74,4 @@ export interface DatepickerPropertyConfig {
     field: string;
     /* if specified, label with this string instead of field */
     label?: string;
-    /* if specified, display the date according to this format */
-    dateFormat?: string;
 }
