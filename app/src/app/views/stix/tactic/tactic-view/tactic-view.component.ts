f5679a7f8c0296a8a3b41b5efd765cc3a7faed3e
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2022-09-12 12:47:13 -0400

    tactic view cleanup

--- app/src/app/views/stix/tactic/tactic-view/tactic-view.component.ts
+++ app/src/app/views/stix/tactic/tactic-view/tactic-view.component.ts
@@ -1,28 +1,23 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Tactic } from 'src/app/classes/stix/tactic';
 import { AuthenticationService } from 'src/app/services/connectors/authentication/authentication.service';
 import { StixViewPage } from '../../stix-view-page';
 import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
 import { StixObject } from 'src/app/classes/stix/stix-object';
 import { Subscription } from 'rxjs';
-import { EditorService } from 'src/app/services/editor/editor.service';
 
 @Component({
     selector: 'app-tactic-view',
     templateUrl: './tactic-view.component.html',
     styleUrls: ['./tactic-view.component.scss']
 })
-export class TacticViewComponent extends StixViewPage implements OnInit, OnDestroy {
+export class TacticViewComponent extends StixViewPage implements OnInit {
     public loading: boolean = false;
     public get tactic(): Tactic { return this.config.object as Tactic; }
     public techniques: StixObject[] = [];
-    public onEditStopSubscription: Subscription;
 
-    constructor(authenticationService: AuthenticationService, private restApiConnector: RestApiConnectorService, private editorService: EditorService) {
+    constructor(authenticationService: AuthenticationService, private restApiConnector: RestApiConnectorService) {
         super(authenticationService);
-        this.onEditStopSubscription = this.editorService.onEditingStopped.subscribe({
-            next: () => { this.getTechniques(); } // reload techniques
-        })
     }
 
     ngOnInit() {
@@ -31,10 +26,6 @@ export class TacticViewComponent extends StixViewPage implements OnInit, OnDestr
         } else {
             this.getTechniques();
         }
-    }
-
-    ngOnDestroy(): void {
-        if (this.onEditStopSubscription) this.onEditStopSubscription.unsubscribe();
     }
 
     /**
