f681d26d2b51025e2496d06c00230ee7a2205ef6
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2022-09-12 12:43:27 -0400

    only load techniques for established tactics

--- app/src/app/views/stix/tactic/tactic-view/tactic-view.component.ts
+++ app/src/app/views/stix/tactic/tactic-view/tactic-view.component.ts
@@ -1,29 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Tactic } from 'src/app/classes/stix/tactic';
 import { AuthenticationService } from 'src/app/services/connectors/authentication/authentication.service';
 import { StixViewPage } from '../../stix-view-page';
 import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
 import { StixObject } from 'src/app/classes/stix/stix-object';
+import { Subscription } from 'rxjs';
+import { EditorService } from 'src/app/services/editor/editor.service';
 
 @Component({
     selector: 'app-tactic-view',
     templateUrl: './tactic-view.component.html',
     styleUrls: ['./tactic-view.component.scss']
 })
-export class TacticViewComponent extends StixViewPage implements OnInit {
+export class TacticViewComponent extends StixViewPage implements OnInit, OnDestroy {
     public loading: boolean = false;
     public get tactic(): Tactic { return this.config.object as Tactic; }
     public techniques: StixObject[] = [];
+    public onEditStopSubscription: Subscription;
 
-    constructor(authenticationService: AuthenticationService, private restApiConnector: RestApiConnectorService) {
+    constructor(authenticationService: AuthenticationService, private restApiConnector: RestApiConnectorService, private editorService: EditorService) {
         super(authenticationService);
+        this.onEditStopSubscription = this.editorService.onEditingStopped.subscribe({
+            next: () => { this.getTechniques(); } // reload techniques
+        })
     }
 
     ngOnInit() {
         if (this.tactic.firstInitialized) {
             this.tactic.initializeWithDefaultMarkingDefinitions(this.restApiConnector);
+        } else {
+            this.getTechniques();
         }
-        this.getTechniques();
+    }
+
+    ngOnDestroy(): void {
+        if (this.onEditStopSubscription) this.onEditStopSubscription.unsubscribe();
     }
 
     /**
