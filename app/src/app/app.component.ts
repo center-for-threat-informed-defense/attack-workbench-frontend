4b0339f2ab1274fa035e14759ade07e57c7713ce
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-12-23 08:41:49 -0500

    migrated all 'light'/'dark' string instances to Theme references; synced mode to OS/browser settings; formatting

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
-import { MatDrawerContainer } from '@angular/material/sidenav';
-import { OverlayContainer } from '@angular/cdk/overlay';
-import { getCookie, setCookie, hasCookie } from "./util/cookies";
-import { SidebarService } from './services/sidebar/sidebar.service';
-import { NGXLogger } from 'ngx-logger';
-import { initLogger } from './util/logger';
-import { AuthenticationService } from './services/connectors/authentication/authentication.service';
-import { NavigationEnd, Router } from '@angular/router';
+import {OnInit, AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
+import {MatDrawerContainer} from '@angular/material/sidenav';
+import {OverlayContainer} from '@angular/cdk/overlay';
+import {getCookie, hasCookie, setCookie} from "./util/cookies";
+import {SidebarService} from './services/sidebar/sidebar.service';
+import {NGXLogger} from 'ngx-logger';
+import {initLogger} from './util/logger';
+import {AuthenticationService} from './services/connectors/authentication/authentication.service';
+import {NavigationEnd, Router} from '@angular/router';
+import {Theme} from './globals';
 
 @Component({
   selector: 'app-root',
@@ -14,24 +15,28 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements AfterViewInit {
-    //drawer container to resize when contents change size
-    @ViewChild(MatDrawerContainer, {static: true}) private container: MatDrawerContainer;
-    //elements for scroll behavior
-    @ViewChild("header", {static: false, read: ElementRef}) private header: ElementRef;
-    @ViewChild("scrollRef", {static: false, read: ElementRef}) private scrollRef: ElementRef;
+export class AppComponent implements AfterViewInit, OnInit {
 
+    // Drawer container to resize when contents change size
+    @ViewChild(MatDrawerContainer, {static: true}) private container: MatDrawerContainer;
+
+    // Elements for scroll behavior
+    @ViewChild('header', { static: false, read: ElementRef }) private header: ElementRef;
+    @ViewChild('scrollRef', { static: false, read: ElementRef }) private scrollRef: ElementRef;
+
+    public theme = Theme.DarkMode; // sets the default theme?
     public alertStatus;
+
     constructor(private overlayContainer: OverlayContainer,
                 private sidebarService: SidebarService,
                 private authenticationService: AuthenticationService,
                 private router: Router,
-                private logger: NGXLogger) { //note: this isn't used directly, but it MUST be imported to work properly
+                private logger: NGXLogger) { // Note: this isn't used directly, but it MUST be imported to work properly
 
-        if (hasCookie("theme")) {
-            this.setTheme(getCookie("theme"))
+        if (hasCookie('theme')) {
+            this.setTheme(getCookie('theme'));
         } else {
-            this.setTheme("light");
+            this.setTheme(Theme.LightMode);
         }
 
         const routerSubscription = this.router.events.subscribe({
@@ -99,25 +104,50 @@ export class AppComponent implements AfterViewInit {
         });
     }
 
-    public theme = "light";
-    //toggle the current theme
-    public toggleTheme() {
-        this.setTheme(this.theme == "light"? "dark" : "light");
+    // Toggle the current theme
+    public toggleTheme(): void {
+        // tslint:disable-next-line:triple-equals
+        this.setTheme(this.theme == Theme.LightMode ? Theme.DarkMode : Theme.LightMode);
     }
 
-    public setTheme(theme: string) {
-        this.logger.log("setting theme to", theme);
+    /**
+     * Sets the initial/default theme based on the operating system/browser settings. Called by ngOnInit.
+     */
+    public setDefaultTheme(): void {
+        // The theme property will be set to 'dark' if the operating system or browser is in dark mode, and 'light' if
+        // it is in light mode
+        const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+        this.theme = mediaQueryList.matches ? Theme.DarkMode : Theme.LightMode;
+
+        // use the addEventListener method of the MediaQueryList object to set up a listener that will be called
+        // whenever the light/dark mode setting changes
+        mediaQueryList.addEventListener('change', (event) => {
+          this.theme = event.matches ? Theme.DarkMode : Theme.LightMode;
+        });
+    }
+
+    public setTheme(theme: string): void {
+        this.logger.log(`Setting theme to ${theme}`);
         this.theme = theme;
         const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
-        overlayContainerClasses.remove("dark", "light");
+        overlayContainerClasses.remove(Theme.DarkMode, Theme.LightMode);
         overlayContainerClasses.add(this.theme);
-        setCookie("theme", theme, 30);
+        setCookie('theme', theme, 30);
+    }
+
+    ngOnInit(): void {
+      this.setDefaultTheme();
     }
 
     // header hiding with scroll
     ngAfterViewInit() {
         this.scrollRef.nativeElement.addEventListener('scroll', (e) => this.adjustHeaderPlacement(), true);
         //to fix rare cases that the page has resized without scroll events triggering, recompute the offset every 5 seconds
+      /**
+       * The code is using the setInterval() function to call the adjustHeaderPlacement() method every 5
+       * seconds. This is likely intended to fix cases where the page has been resized without scroll events being
+       * triggered, in which case the header placement may need to be adjusted.
+       */
         setInterval(() => this.adjustHeaderPlacement(), 5000);
     }
     ngOnDestroy() {
