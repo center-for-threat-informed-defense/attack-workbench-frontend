7bf950cd2a23db657bb73b2f29923d23d8ecb6a5
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-01-20 15:33:03 -0500

    formatting

--- app/src/app/views/stix/stix-view-page.ts
+++ app/src/app/views/stix/stix-view-page.ts
@@ -6,17 +6,17 @@ import { StixDialogComponent } from './stix-dialog/stix-dialog.component';
 
 @Component({template: ''})
 export abstract class StixViewPage {
-    constructor(private authenticationService: AuthenticationService) { }
-
-    //configuration for the view page behavior
-    @Input() public config: StixViewConfig;
-    public get editing(): boolean { return this.config.mode == "edit"; }
+    constructor(private authenticationService: AuthenticationService) {}
+    public get editing(): boolean { return this.config.mode == 'edit'; }
     public get canEdit(): boolean { return this.authenticationService.canEdit(); }
 
-    //outputs to use if config.sidebarControl == "events"
+    // configuration for the view page behavior
+    @Input() public config: StixViewConfig;
+
+    // outputs to use if config.sidebarControl == "events"
     @Output() public onOpenHistory = new EventEmitter();
-    public openHistory():void { this.onOpenHistory.emit(); }
     @Output() public onOpenNotes = new EventEmitter();
+    public openHistory(): void { this.onOpenHistory.emit(); }
     public openNotes(): void { this.onOpenNotes.emit(); }
 }
 
@@ -26,14 +26,14 @@ export interface StixViewConfig {
     *    edit: editing the object
     *    diff: displaying the diff between two STIX objects. If this mode is selected, two StixObjects must be specified in the objects field
     */
-    mode?: "view" | "diff" | "edit";
+    mode?: 'view' | 'diff' | 'edit';
     /* The object to show on the page
      * Note: if mode is diff, pass an array of two objects to diff
      */
-    object: StixObject | StixObject[]
+    object: StixObject | StixObject[];
     /* if true or omitted, show relationships with the object on the page. If false, omit the relationships */
     showRelationships?: boolean;
-    /* is the current page editable? 
+    /* is the current page editable?
      * if true or omitted, include edit elements on the page such as buttons to add a relationship
      * if false, hide such elements
      */
@@ -45,6 +45,6 @@ export interface StixViewConfig {
      * if "events", sidebar-related controls should be passed as an @output event.
      * if "service", use sidebar.service to control the sidebar
      */
-    sidebarControl?: "disable" | "events" | "service"
-    dialog?: MatDialogRef<StixDialogComponent> // reference to the current dialog
+    sidebarControl?: 'disable' | 'events' | 'service';
+    dialog?: MatDialogRef<StixDialogComponent>; // reference to the current dialog
 }
