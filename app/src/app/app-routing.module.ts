a15d898cb1f7839a42025e9983067b9bddca9492
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-03-13 14:51:08 -0400

    add profile page route

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AdminPageComponent } from './views/admin-page/admin-page.component';
 import { OrgSettingsPageComponent } from './views/admin-page/org-settings-page/org-settings-page.component';
 import { UserAccountsPageComponent } from './views/admin-page/user-accounts-page/user-accounts-page.component';
 import { DefaultMarkingDefinitionsComponent } from './views/admin-page/default-marking-definitions/default-marking-definitions.component';
+import { ProfilePageComponent } from './views/profile-page/profile-page.component';
 import { AuthorizationGuard } from './services/helpers/authorization.guard';
 import { Role } from './classes/authn/role';
 
@@ -138,6 +139,16 @@ const routes: Routes = [
                         "component": HelpPageComponent,
                     },
                 ]
+            },
+            {
+                "path": "profile",
+                "canActivate": [AuthorizationGuard],
+                "data": {
+                    "breadcrumb": "profile",
+                    "title": "Profile",
+                    "roles": [Role.VISITOR, Role.EDITOR, Role.ADMIN]
+                },
+                "component": ProfilePageComponent
             },
         ]
     },
