313eca2213707480fb208c5509e495f7753fd63b
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-03-14 15:06:54 -0400

    init list of recent activity

--- app/src/app/views/profile-page/profile-page.component.ts
+++ app/src/app/views/profile-page/profile-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { StixObject } from 'src/app/classes/stix/stix-object';
 import { AuthenticationService } from 'src/app/services/connectors/authentication/authentication.service';
+import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
 
 @Component({
     selector: 'app-profile-page',
@@ -9,12 +11,34 @@ import { AuthenticationService } from 'src/app/services/connectors/authenticatio
 })
 export class ProfilePageComponent implements OnInit {
     public get user() { return this.authenticationService.currentUser; }
+    public recentActivity: StixObject[];
+    public reload: boolean = false;
+    public data$;
 
-    constructor(private authenticationService: AuthenticationService) {
+    constructor(private authenticationService: AuthenticationService, private restApiService: RestApiConnectorService) {
         // intentionally left blank
     }
 
     ngOnInit(): void {
-        // intentionally left blank
+        this.loadRecentActivity();
+    }
+
+    public loadRecentActivity() {
+        this.data$ = this.restApiService.getAllObjects(null, null, null, null, true, true, true);
+        let subscription = this.data$.subscribe({
+            next: (results) => {
+                let stixObjects = results.data as StixObject[];
+                this.recentActivity = stixObjects.filter(sdo => {
+                    if (sdo.type == 'relationship') return false; // # TODO unfilter relationships
+                    if (sdo.workflow && sdo.workflow.created_by_user_account) {
+                        return sdo.workflow.created_by_user_account == this.user.id;
+                    }
+                    return false;
+                });
+                this.reload = true;
+                window.setTimeout(() => this.reload = false)
+            },
+            complete: () => { subscription.unsubscribe(); }
+        })
     }
 }
