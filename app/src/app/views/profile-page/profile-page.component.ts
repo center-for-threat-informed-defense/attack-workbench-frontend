f92b980f500a2bb5ab481b67275cfde2dccd29ee
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-03-14 11:34:25 -0400

    display user jdenticon

--- app/src/app/views/profile-page/profile-page.component.ts
+++ app/src/app/views/profile-page/profile-page.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/connectors/authentication/authentication.service';
 
 @Component({
     selector: 'app-profile-page',
     templateUrl: './profile-page.component.html',
-    styleUrls: ['./profile-page.component.scss']
+    styleUrls: ['./profile-page.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class ProfilePageComponent implements OnInit {
+    public get user() { return this.authenticationService.currentUser; }
 
-    constructor() { }
-
-    ngOnInit(): void {
+    constructor(private authenticationService: AuthenticationService) {
+        // intentionally left blank
     }
 
+    ngOnInit(): void {
+        // intentionally left blank
+    }
 }
