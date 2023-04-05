14f527d4d3062b7cd81f5eb4cf4aed2c2e0ea079
Vincent Minnocci <vminnocci@mitre.org>
2023-04-05 15:57:08 -0400

    rework current user lists to user new components

--- app/src/app/views/admin-page/teams/teams-view-page/teams-view-page.component.ts
+++ app/src/app/views/admin-page/teams/teams-view-page/teams-view-page.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnDestroy, OnInit,ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
-import { UserAccount } from 'src/app/classes/authn/user-account';
-import { Paginated, RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
+import { Subscription } from 'rxjs';
+import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
 import { EditorService } from 'src/app/services/editor/editor.service';
 import { MatDialog } from '@angular/material/dialog';
 import { AddDialogComponent } from 'src/app/components/add-dialog/add-dialog.component';
+import { AddUsersDialogComponent } from 'src/app/components/add-users-dialog/add-users-dialog.component';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-teams-view-page',
@@ -14,19 +14,12 @@ import { AddDialogComponent } from 'src/app/components/add-dialog/add-dialog.com
   styleUrls: ['./teams-view-page.component.scss']
 })
 export class TeamsViewPageComponent implements OnInit,OnDestroy {
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  public columnsToDisplay: string[];
   public editing: boolean;
   public team = null;
-  public users: Array<UserAccount>;
-  public totalObjectCount: number;
   private subscriptions: Array<Subscription> = [];
   private teamId = '';
-  private users$: Observable<Paginated<UserAccount>>;
-  private userSubscription: Subscription;
 
   constructor(private restAPIConnector: RestApiConnectorService, private route: ActivatedRoute, private editorService: EditorService, private router:Router, private dialog: MatDialog) { 
-    this.columnsToDisplay = ['username', 'email', 'options'];
   }
 
   ngOnInit(): void {
@@ -46,9 +39,9 @@ export class TeamsViewPageComponent implements OnInit,OnDestroy {
       this.router.navigate([this.route.parent.url], {});
     }));
     this.subscriptions.push(this.editorService.onEditingStopped.subscribe(()=>{
-      this.reload();
+      this.loadTeam();
     }));
-    this.reload();
+    this.loadTeam();
   }
 
   ngOnDestroy(): void {
@@ -57,50 +50,32 @@ export class TeamsViewPageComponent implements OnInit,OnDestroy {
     }
   }
 
-  public reload(): void {
+  public loadTeam(): void {
     this.team = this.restAPIConnector.getTeam(this.teamId);
-    this.loadUsers();
-  }
-
-  public loadUsers():void {
-    let limit = this.paginator ? this.paginator.pageSize : 10;
-    let offset = this.paginator ? this.paginator.pageIndex * limit : 0;
-    this.users$ = this.restAPIConnector.getUserAccountsByTeamId(this.teamId,{limit, offset});
-    this.userSubscription = this.users$.subscribe({
-      next: (res) => {
-          this.users = res.data.filter((user)=>this.team.users.includes(user.id));
-          this.totalObjectCount = res.pagination.total;
-      },
-      complete: () => {
-          this.userSubscription.unsubscribe();
-      }
-    })
-  }
-
-  public removeUser(element): void {
-    this.team.users = this.team.users.filter((user)=>user!==element.id);
-    this.restAPIConnector.putTeam(this.team);
-    this.reload();
   }
 
   public updateUsers(): void {
-    let prompt = this.dialog.open(AddDialogComponent, {
+    let prompt = this.dialog.open(AddUsersDialogComponent, {
       maxWidth: "40em",
       minWidth: "40em",
       disableClose: true,
       autoFocus: false, // disables auto focus on the dialog form field
       data: {
-        selectableObjects: this.users,
-        type: 'users',
-        select: 'something',
-        selectionType: 'many',
+        selectedUserIds: this.team.users,
+        select: new SelectionModel<string>(),
         title: `Select users you wish to be in this team`,
-        buttonLabel: "OK"
+        buttonLabel: "CONFIRM",
+        clearSelection: true,
       },
     });
     let subscription = prompt.afterClosed().subscribe({
         next: (response) => {
-            console.log(response);
+            console.log(this.team.users)
+            if (response) {
+              this.team.users = response;
+              this.restAPIConnector.putTeam(this.team);
+              this.router.navigate([]);
+            }
         },
         complete: () => { subscription.unsubscribe(); } //prevent memory leaks
     });
