600d4cf686da6d91adf13f5faf39019b4aafdc1e
vince-a-m <126682360+vince-a-m@users.noreply.github.com>
2023-05-04 14:04:54 -0400

    Feature/search by users (WB-42) (#464)
    * add in list and dialog components
    
    * add lastUpdatedBy to rest api service
    
    * fix stix list to show the users selection modal
    
    * add user selection option to list components
    
    * updated changelog
    
    * added comments to users list
    
    * migrate add users dialog into existing add dialog
    
    * better if check in add dialog
    
    * switch from AddUsersDialog to AddDialog
    
    * code smell fixes
    
    * fixes related to pr comments
    
    * fixes related to comments on PR

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -77,6 +77,7 @@ import { CollectionUpdateDialogComponent } from './components/collection-update-
 
 // STIX components
 import { StixListComponent } from './components/stix/stix-list/stix-list.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 import { ExternalReferencesPropertyComponent } from "./components/stix/external-references-property/external-references-property.component";
 import { ExternalReferencesViewComponent } from './components/stix/external-references-property/external-references-view/external-references-view.component';
@@ -307,7 +308,8 @@ import { NotesSearchListComponent } from './views/notes-page/notes-search-list/n
     CitationPropertyComponent,
     CitationViewComponent,
     CitationEditComponent,
-    NotesSearchListComponent
+    NotesSearchListComponent,
+    UsersListComponent
   ],
   imports: [
     BreadcrumbModule,
