41858598598a66f504826823da00f76b585c555f
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-05-09 13:16:57 -0400

    hide object status from toolbar for home routes

--- app/src/app/components/toolbar/toolbar.component.ts
+++ app/src/app/components/toolbar/toolbar.component.ts
@@ -22,6 +22,7 @@ export class ToolbarComponent implements OnInit {
 
     public get editing(): boolean { return this.editorService.editing; }
     public get editable(): boolean { return this.editorService.editable; }
+    public get hasStatus(): boolean { return this.editorService.hasStatus; }
     public get hasRelationships(): boolean { return this.editorService.hasRelationships; }
     public get deletable(): boolean { return this.editorService.deletable && this.authenticationService.canDelete(); }
 
