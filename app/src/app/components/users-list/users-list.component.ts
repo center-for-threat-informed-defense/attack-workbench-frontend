9fcd2311e49bd309605436b4d011d69125236e86
Vincent Minnocci <vminnocci@mitre.org>
2023-05-09 08:41:29 -0400

    showSearch and filters default to true

--- app/src/app/components/users-list/users-list.component.ts
+++ app/src/app/components/users-list/users-list.component.ts
@@ -35,14 +35,14 @@ export class UsersListComponent implements OnInit {
    * Whether or not to show the search
    */
   public get showSearch(): boolean {
-    return this.config && this.config.showSearch ? this.config.showSearch : false;
+    return this.config ? this.config.showSearch : true;
   }
 
   /**
    * Whether or not to show filters
    */
   public get showFilters(): boolean {
-    return this.config && this.config.showFilters ? this.config.showFilters : false;
+    return this.config ? this.config.showFilters : true;
   }
 
   /**
@@ -222,10 +222,10 @@ export interface UsersListConfig {
   // team:Team,
   team: any,
   // whether or not to display the search bar
-  // defaults to false 
+  // defaults to true 
   showSearch: boolean,
   // whether or not to show the filters in the list
-  // defaults to false
+  // defaults to true
   showFilters: boolean,
   // mode the list is being used 'view' or 'select'
   // default is 'view'
