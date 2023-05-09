014cc24521b55b98aa4bb0266121187685499bfc
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-05-09 13:52:40 -0400

    save changes to user profile

--- app/src/app/views/profile-page/profile-page.component.ts
+++ app/src/app/views/profile-page/profile-page.component.ts
@@ -1,6 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { UserAccount } from 'src/app/classes/authn/user-account';
 import { AuthenticationService } from 'src/app/services/connectors/authentication/authentication.service';
+import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
 import { EditorService } from 'src/app/services/editor/editor.service';
 
 @Component({
@@ -9,13 +12,35 @@ import { EditorService } from 'src/app/services/editor/editor.service';
     styleUrls: ['./profile-page.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit, OnDestroy {
+    private saveSubscription: Subscription;
+
     public get user(): UserAccount { return this.authenticationService.currentUser; }
     public get editing(): boolean { return this.editorService.editing; }
     public get canEdit(): boolean { return this.authenticationService.canEdit(); }
 
     constructor(private authenticationService: AuthenticationService,
-                private editorService: EditorService) {
+                private editorService: EditorService,
+                private restApiService: RestApiConnectorService,
+                private router: Router) {
         // intentionally left blank
+    }
+
+    ngOnInit(): void {
+        this.saveSubscription = this.editorService.onSave.subscribe({
+            next: (_event) => this.save()
+        });
+    }
+
+    ngOnDestroy() {
+        this.saveSubscription.unsubscribe();
+    }
+
+    /** save profile changes */
+    private save(): void {
+        let saveUser = this.user.save(this.restApiService).subscribe({
+            next: (_saveResult) => this.router.navigate(["/profile"]),
+            complete: () => { saveUser.unsubscribe(); }
+        })
     }
 }
