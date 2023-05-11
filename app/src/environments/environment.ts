0d6751ff736b6cf82d320197c327de511505452b
Vincent Minnocci <vminnocci@mitre.org>
2023-05-11 10:43:00 -0400

    update env files

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -24,6 +24,12 @@ export const environment = {
             // https://github.com/center-for-threat-informed-defense/attack-workbench-collection-manager
             enabled: true, //if false, all systems for collection management will be disabled
             url: "http://localhost:3001/cm-api"
+        },
+        attack_website: {
+          // configuration for the ATT&CK Website
+          // https://github.com/mitre-attack/attack-website
+          enabled: true, // if false, the attack website integration will not work
+          url: "http://localhost:4900"
         }
     }
 };
