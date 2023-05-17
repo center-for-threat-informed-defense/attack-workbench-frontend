7bae47000748f595b510b176335d5c87447695b3
arobbins <arobbins@mitre.org>
2023-05-17 11:53:30 -0400

    call new matrix endpoint (refactor matrix view)

--- app/src/app/views/stix/matrix/matrix-view/matrix-view.component.ts
+++ app/src/app/views/stix/matrix/matrix-view/matrix-view.component.ts
@@ -15,40 +15,22 @@ import { AuthenticationService } from 'src/app/services/connectors/authenticatio
 export class MatrixViewComponent extends StixViewPage implements OnInit {
     public all_tactics: Array<StixObject>;
 
-    public view: String = "flat";
+    public view: String = "side";
     public get matrix(): Matrix { return this.config.object as Matrix; }
-
+    public thisMatrix : Matrix;
     public tacticList = [];
-    public matrixMap: Map<string, Technique[]>;
 
     constructor(private restAPIConnectorService: RestApiConnectorService, authenticationService: AuthenticationService) {
         super(authenticationService);
     }
 
     ngOnInit() {
-      this.matrixMap = new Map<string, Technique[]>();
 
         if (!this.config.hasOwnProperty('showRelationships') || this.config.showRelationships) {
-            let subscription = this.restAPIConnectorService.getAllTactics().subscribe({
-                next: (all_tactics) => {
-                  this.all_tactics = all_tactics.data;
-                  // sort tactics according to the order of attack website
-                  this.matrix.tactic_refs.forEach(item => {
-                    this.tacticList.push(all_tactics.data.find(e => e.stixID === item))
-                  });
-                  this.tacticList.forEach((tactic) => {
-                    let itemsCopy;
-                    let subscription = this.restAPIConnectorService.getTechniquesInTactic(tactic.stixID, tactic.modified).subscribe({
-                      next: (items) => {
-                        // alphabetically order subtechniques
-                        itemsCopy = items.sort((a,b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()))
-                        this.matrixMap.set(tactic.stixID, itemsCopy)
-                      },
-                      complete: () => {
-                        subscription.unsubscribe();
-                      }
-                    })
-                  })
+            let subscription = this.restAPIConnectorService.getTechniquesInMatrix(this.matrix, this.matrix.stixID, this.matrix.modified).subscribe({
+                next: (data) => {
+                  console.log(data)
+                  this.thisMatrix = data;
                 },
                 complete: () => { subscription.unsubscribe(); } //prevent memory leaks
             })
@@ -59,8 +41,8 @@ export class MatrixViewComponent extends StixViewPage implements OnInit {
 
     }
     public toggleAllSubtechniquesVisible(value: boolean) {
-      for (let entry of this.matrixMap.entries()) {
-        for (let technique of entry[1])
+      for (let tactic of this.matrix.tactic_objects) {
+        for (let technique of tactic.technique_objects)
         {
           technique.show_subtechniques = value
         }
