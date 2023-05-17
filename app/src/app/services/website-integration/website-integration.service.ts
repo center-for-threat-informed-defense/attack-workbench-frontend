aca58242ec8d89bcdd6c7a21a1036baed5c4f9e7
Vincent Minnocci <vminnocci@mitre.org>
2023-05-17 13:37:18 -0400

    allow a stixId to be passed in instead of read from the editor service

--- app/src/app/services/website-integration/website-integration.service.ts
+++ app/src/app/services/website-integration/website-integration.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 })
 export class WebsiteIntegrationService {
   private get websiteIntegrationUrl(): string { return environment.integrations.attack_website.url; }
-  private get websiteIntegrationStatus(): boolean { return environment.integrations.attack_website.enabled; }
+  private get websiteIntegrationEnabled(): boolean { return environment.integrations.attack_website.enabled; }
 
   private typeToRouteMap = {
     'software': 'software',
@@ -24,73 +24,70 @@ export class WebsiteIntegrationService {
   
   constructor(private editorService:EditorService, private restAPIService: RestApiConnectorService) { }
 
-  public checkExternalUrlValidity() {
-    if (this.websiteIntegrationStatus == false) {
-      this.currentWebIntegrationStatus = {url:null,stixId:null,valid:false};
-      return this.currentWebIntegrationStatus.valid;
+  public checkExternalUrlValidity(stixIdToCheck) {
+    if (this.websiteIntegrationEnabled == false) {
+      return false;
     }
     let data$: any;
-    if (this.editorService.stixId) { 
-      if (this.editorService.stixId == "new") {
-        this.currentWebIntegrationStatus = {url:null,stixId:null,valid:false};
-        return this.currentWebIntegrationStatus.valid;
-      }
-      if (this.editorService.stixId == this.currentWebIntegrationStatus.stixId) {
-        return this.currentWebIntegrationStatus.valid;
-      }
-      // retrieve object
-      if (this.editorService.type == "software") data$ = this.restAPIService.getSoftware(this.editorService.stixId);
-      else if (this.editorService.type == "group") data$ = this.restAPIService.getGroup(this.editorService.stixId);
-      else if (this.editorService.type == "matrix") data$ = this.restAPIService.getMatrix(this.editorService.stixId);
-      else if (this.editorService.type == "mitigation") data$ = this.restAPIService.getMitigation(this.editorService.stixId);
-      else if (this.editorService.type == "tactic") data$ = this.restAPIService.getTactic(this.editorService.stixId);
-      else if (this.editorService.type == "campaign") data$ = this.restAPIService.getCampaign(this.editorService.stixId);
-      else if (this.editorService.type == "technique") data$ = this.restAPIService.getTechnique(this.editorService.stixId);
-      else if (this.editorService.type == "data-source") data$ = this.restAPIService.getDataSource(this.editorService.stixId);
-      else {
-        this.currentWebIntegrationStatus = {url:null,stixId:null,valid:false};
-        return this.currentWebIntegrationStatus.valid;
-      }
-      let objSubscription = data$.subscribe({
-          next: (data) => {
-              if (data.length !== 1) {
-                this.currentWebIntegrationStatus = {url:null,stixId:null,valid:false};
+    if (!stixIdToCheck) {
+      return false;
+    }
+    if (stixIdToCheck == "new") {
+      return false;
+    }
+    if (this.currentWebIntegrationStatus.stixId == stixIdToCheck) {
+      return this.currentWebIntegrationStatus.valid;
+    }
+    this.currentWebIntegrationStatus = {url:null, valid: false, stixId: stixIdToCheck};
+    // retrieve object
+    if (this.editorService.type == "software") data$ = this.restAPIService.getSoftware(stixIdToCheck);
+    else if (this.editorService.type == "group") data$ = this.restAPIService.getGroup(stixIdToCheck);
+    else if (this.editorService.type == "matrix") data$ = this.restAPIService.getMatrix(stixIdToCheck);
+    else if (this.editorService.type == "mitigation") data$ = this.restAPIService.getMitigation(stixIdToCheck);
+    else if (this.editorService.type == "tactic") data$ = this.restAPIService.getTactic(stixIdToCheck);
+    else if (this.editorService.type == "campaign") data$ = this.restAPIService.getCampaign(stixIdToCheck);
+    else if (this.editorService.type == "technique") data$ = this.restAPIService.getTechnique(stixIdToCheck);
+    else if (this.editorService.type == "data-source") data$ = this.restAPIService.getDataSource(stixIdToCheck);
+    else {
+      return false;
+    }
+    let objSubscription = data$.subscribe({
+        next: (data) => {
+            if (data.length !== 1) {
+              return false;
+            }
+            const attackObject = data[0];
+            const attackID = attackObject.attackID.split('.');
+            let additionalPath = '';
+            if (this.editorService.type == "matrix") {
+              if (attackID == 'enterprise-attack') {
+                additionalPath = '/enterprise/';
+              } else if (attackID == 'mobile-attack') {
+                additionalPath = '/mobile';
+              } else if (attackID == 'ics-attack') {
+                additionalPath = '/ics';
+              }
+            } else {
+              for (let i = 0; i < attackID.length; i++) {
+                additionalPath = additionalPath.concat('/',attackID[i]);
+              }
+            }
+            const url = `${this.websiteIntegrationUrl}/${this.typeToRouteMap[this.editorService.type]}${additionalPath}`;
+            this.currentWebIntegrationStatus.url = url;
+            // with no-cors we will get no data but we can validate that the URL is valid
+            fetch(url, {mode: 'no-cors'}).then(
+              (response) => {
+                this.currentWebIntegrationStatus.valid = true;
+                return this.currentWebIntegrationStatus.valid;
+              },
+              (error) => {
+                this.currentWebIntegrationStatus.valid = false;
                 return this.currentWebIntegrationStatus.valid;
               }
-              const attackObject = data[0];
-              const attackID = attackObject.attackID.split('.');
-              let additionalPath = '';
-              if (this.editorService.type == "matrix") {
-                if (attackID == 'enterprise-attack') {
-                  additionalPath = '/enterprise/';
-                } else if (attackID == 'mobile-attack') {
-                  additionalPath = '/mobile';
-                } else if (attackID == 'ics-attack') {
-                  additionalPath = '/ics';
-                }
-              } else {
-                for (let i = 0; i < attackID.length; i++) {
-                  additionalPath = additionalPath.concat('/',attackID[i]);
-                }
-              }
-              const url = `${this.websiteIntegrationUrl}/${this.typeToRouteMap[this.editorService.type]}${additionalPath}`;
-              this.currentWebIntegrationStatus.url = url;
-              this.currentWebIntegrationStatus.stixId = this.editorService.stixId;
-              // with no-cors we will get no data but we can validate that the URL is valid
-              fetch(url, {mode: 'no-cors'}).then(
-                (response) => {
-                  this.currentWebIntegrationStatus.valid = true;
-                  return this.currentWebIntegrationStatus.valid;
-                },
-                (error) => {
-                  this.currentWebIntegrationStatus.valid = false;
-                  return this.currentWebIntegrationStatus.valid;
-                }
-              );
-          },
-          complete: () => { objSubscription.unsubscribe() }
-      });
-    }
+            );
+        },
+        complete: () => { objSubscription.unsubscribe() }
+    });
   }
 }
 
