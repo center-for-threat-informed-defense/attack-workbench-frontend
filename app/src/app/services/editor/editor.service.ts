919923edc42b7b41b030f68f5f1193c3013aab2e
Vincent Minnocci <vminnocci@mitre.org>
2023-05-23 10:27:07 -0400

    improve isGroup logic

--- app/src/app/services/editor/editor.service.ts
+++ app/src/app/services/editor/editor.service.ts
@@ -40,7 +40,13 @@ export class EditorService {
                 let editable = this.getEditableFromRoute(this.router.routerState, this.router.routerState.root);
                 let attackType = this.route.root.firstChild.snapshot.data.breadcrumb;
                 this.editable = editable.length > 0 && editable.every(x => x) && this.authenticationService.canEdit(attackType);
-                this.isGroup = this.type === 'group';
+                try {
+                  // if we have a group type and it is NOT new we can create a collection from all of the objects in the group
+                  this.isGroup = this.type === 'group' && this.stixId != 'new';
+                } catch (err) {
+                  // stixId throws an error when we are on the list page so this catches that
+                  this.isGroup = false;
+                }
                 this.hasWorkflow = attackType !== 'home';
                 if (!(this.editable && this.hasWorkflow)) this.sidebarService.currentTab = "references";
                 this.sidebarService.setEnabled("history", this.editable && this.hasWorkflow);
