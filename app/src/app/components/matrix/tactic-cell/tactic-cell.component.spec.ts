d88d05af1374616f8c5ecb5e4039d65b5b70644a
arobbins <arobbins@mitre.org>
2023-05-25 13:06:22 -0400

    remove unneccessary comments

--- app/src/app/components/matrix/tactic-cell/tactic-cell.component.spec.ts
+++ app/src/app/components/matrix/tactic-cell/tactic-cell.component.spec.ts
@@ -1,4 +1,3 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TacticCellComponent } from './tactic-cell.component';
--- app/src/app/components/matrix/technique-cell/technique-cell.component.spec.ts
+++ app/src/app/components/matrix/technique-cell/technique-cell.component.spec.ts
@@ -1,4 +1,3 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TechniqueCellComponent } from './technique-cell.component';
