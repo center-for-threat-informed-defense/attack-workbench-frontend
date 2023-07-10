47a2f41e5a72ac76d052b31ca01f3465b72098e2
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-07-10 11:49:16 -0400

    intial asset view/edit page

--- app/src/app/views/stix/asset/asset-view/asset-view.component.ts
+++ app/src/app/views/stix/asset/asset-view/asset-view.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Asset } from 'src/app/classes/stix';
+import { AuthenticationService } from 'src/app/services/connectors/authentication/authentication.service';
+import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
+import { StixViewPage } from '../../stix-view-page';
 
 @Component({
 	selector: 'app-asset-view',
 	templateUrl: './asset-view.component.html',
 	styleUrls: ['./asset-view.component.scss']
 })
-export class AssetViewComponent implements OnInit {
+export class AssetViewComponent extends StixViewPage implements OnInit {
+	@Output() public onReload = new EventEmitter();
+	public get asset(): Asset { return this.config.object as Asset; }
 
-	constructor() { }
-
-	ngOnInit(): void {
+	constructor(authenticationService: AuthenticationService, private restApiConnector: RestApiConnectorService) {
+		super(authenticationService)
 	}
 
+	ngOnInit(): void {
+		if (this.asset.firstInitialized) {
+			this.asset.initializeWithDefaultMarkingDefinitions(this.restApiConnector);
+		}
+	}
 }
