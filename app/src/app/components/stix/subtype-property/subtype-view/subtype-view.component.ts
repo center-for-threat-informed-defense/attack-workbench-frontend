d8ece34bbba8e66a33082d8a7b2fe564ce1f61a6
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-07-13 08:18:22 -0400

    initi related assets view component

---
+++ app/src/app/components/stix/subtype-property/subtype-view/subtype-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subtype-view',
+  templateUrl: './subtype-view.component.html',
+  styleUrls: ['./subtype-view.component.scss']
+})
+export class SubtypeViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
