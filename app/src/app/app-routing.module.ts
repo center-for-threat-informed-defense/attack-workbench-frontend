bd1b699c5e1b8a71a7ca0c2c14a28e850b6cb19a
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-07-13 08:45:07 -0400

    default landing page fixes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
         },
         "children": [
             {
-                "path": "home",
+                "path": "",
                 "data": {
                     "breadcrumb": "welcome"
                 },
