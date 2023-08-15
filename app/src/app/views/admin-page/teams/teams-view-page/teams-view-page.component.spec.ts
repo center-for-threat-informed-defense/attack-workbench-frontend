a361dfc97305f9f9a33834f1777aaafb9c737f09
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-08-15 11:44:39 -0400

    update formatting

--- app/src/app/views/admin-page/teams/teams-view-page/teams-view-page.component.spec.ts
+++ app/src/app/views/admin-page/teams/teams-view-page/teams-view-page.component.spec.ts
@@ -1,25 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TeamsViewPageComponent } from './teams-view-page.component';
 
 describe('TeamsViewPageComponent', () => {
-  let component: TeamsViewPageComponent;
-  let fixture: ComponentFixture<TeamsViewPageComponent>;
+	let component: TeamsViewPageComponent;
+	let fixture: ComponentFixture<TeamsViewPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TeamsViewPageComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [TeamsViewPageComponent]
+		})
+			.compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TeamsViewPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(TeamsViewPageComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
