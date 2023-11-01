ee95ce1fe11ce5e5c3f5a1f1e135d1aaa4e37083
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-11-01 13:39:31 -0400

    init contributors page

---
+++ app/src/app/views/contributors-page/contributors-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-contributors-page',
+	templateUrl: './contributors-page.component.html',
+	styleUrls: ['./contributors-page.component.scss'],
+})
+export class ContributorsPageComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
