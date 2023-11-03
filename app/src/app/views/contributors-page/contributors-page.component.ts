78f08919990544421ecfbe8fafd0157ef36ea9c1
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-11-03 11:14:13 -0400

    Add ability to edit contributors from contributor dialog  (#516)
    * display number of contributions
    
    * fix alignment
    
    * add state icon to default lists
    
    * move dialog template to new component
    
    * remove style
    
    * reset on reload
    
    * edit contributor and update across objects
    
    * validation
    
    * refetch objects on contributor edit
    
    * template for no results
    
    * update styles and fn docs
    
    * stop editing on save

--- app/src/app/views/contributors-page/contributors-page.component.ts
+++ app/src/app/views/contributors-page/contributors-page.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 import { StixObject } from 'src/app/classes/stix';
+import { ContributorEditDialogComponent } from 'src/app/components/contributor-edit-dialog/contributor-edit-dialog.component';
 import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/rest-api-connector.service';
 
 @Component({
@@ -9,28 +11,38 @@ import { RestApiConnectorService } from 'src/app/services/connectors/rest-api/re
 	styleUrls: ['./contributors-page.component.scss']
 })
 export class ContributorsPageComponent implements OnInit {
-    @ViewChild('contributorsDialog') public contributorsDialog: TemplateRef<any>;
-
     private contributors: string[] = [];
     private numColumns: number = 3;
-    private contributorMap: Map<string, StixObject[]> = new Map(); // contributor -> list of STIX objects they contributed to
+    private contributorMap: Map<string, StixObject[]>; // contributor -> list of STIX objects they contributed to
+    private dialogSaveSubscription: Subscription;
     public loading: boolean = true;
-    public columns: string[][] = [];
-    public dialogRef;
+    public columns: string[][];
 
 	constructor(private restApiConnector: RestApiConnectorService, public dialog: MatDialog) { }
 
 	ngOnInit(): void {
+        this.load();
+    }
+
+    ngOnDestroy(): void {
+        if (this.dialogSaveSubscription) this.dialogSaveSubscription.unsubscribe();
+    }
+
+    /**
+     * Load all objects and parse contributors
+     */
+    private load(): void {
         this.loading = true;
+        this.contributorMap = new Map();
         let subscription = this.restApiConnector.getAllObjects(null, null, null, null, true, true, true).subscribe({
             next: (results) => {
                 let contributors = new Set<string>();
                 results.data.forEach(obj => {
-                    if (obj.contributors) {
+                    if (obj.contributors?.length) {
                         obj.contributors.forEach(contributor => {
                             contributors.add(contributor);
-                            if (contributor in this.contributorMap) {
-                                this.contributorMap[contributor].push(obj);
+                            if (this.contributorMap.has(contributor)) {
+                                this.contributorMap.get(contributor).push(obj);
                             } else {
                                 this.contributorMap.set(contributor, [obj]);
                             }
@@ -45,7 +57,16 @@ export class ContributorsPageComponent implements OnInit {
                 this.loading = false;
                 if (subscription) subscription.unsubscribe();
             }
-        })
+        });
+    }
+
+    /**
+     * Get the number of contributions made by the given contributor
+     * @param contributor the contributor
+     */
+    public getNumContributions(contributor: string): number {
+        let contributions = this.contributorMap.get(contributor);
+        return contributions ? contributions.length : 0;
     }
 
     /**
@@ -60,6 +81,7 @@ export class ContributorsPageComponent implements OnInit {
      * @param numColumns number of columns to display
      */
     public toColumns(numColumns: number) {
+        this.columns = [];
         let chunk = Math.ceil(this.contributors.length / numColumns);
         for (let i = 0; i < numColumns; i++) {
             let start = i * chunk;
@@ -72,12 +94,36 @@ export class ContributorsPageComponent implements OnInit {
      * @param contributor the contributor
      */
     public openDialog(contributor: string): void {
-        this.dialogRef = this.dialog.open(this.contributorsDialog, {
+        // open dialog
+        let prompt = this.dialog.open(ContributorEditDialogComponent, {
             width: '50em',
             data: {
                 contributor: contributor,
                 objects: this.contributorMap.get(contributor)
             }
+        });
+        // subscribe to contributor changes
+        this.dialogSaveSubscription = prompt.componentInstance.onSave.subscribe((newContributor) => {
+            let contributions = this.contributorMap.get(contributor);
+            if (this.contributorMap.has(newContributor)) {
+                this.contributorMap.get(newContributor).push(...contributions);
+            } else {
+                this.contributorMap.set(newContributor, contributions);
+            }
+            // update dialog data
+            prompt.componentInstance.config = {
+                contributor: newContributor,
+                objects: this.contributorMap.get(newContributor)
+            }
+        })
+        // re-fetch values if an edit occurs
+        let subscription = prompt.afterClosed().subscribe({
+            next: (_result) => {
+                if (prompt.componentInstance.dirty) {
+                    this.load();
+                }
+            },
+            complete: () => subscription.unsubscribe()
         });
     }
 }
