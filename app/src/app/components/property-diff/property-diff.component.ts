3748e417b12fec2ac87d8d5eead2518153983889
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2025-01-29 18:46:40 -0500

    property diff component

---
+++ app/src/app/components/property-diff/property-diff.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { DiffMatchPatch } from 'diff-match-patch-ts';
+
+@Component({
+  selector: 'app-property-diff',
+  templateUrl: './property-diff.component.html'
+})
+export class PropertyDiffComponent implements OnInit  {
+  @Input() public config: PropertyDiffConfig;
+  public prettyHtmlDiff: SafeHtml;
+
+  constructor(public sanitizer: DomSanitizer) {
+    // intentionally left blank
+  }
+
+  ngOnInit(): void {
+    console.log('** property diff component', this.config)
+    this.generateDiffHtml(this.config.before, this.config.after)
+  }
+
+  public generateDiffHtml(newValue, oldValue) {
+    const dmp = new DiffMatchPatch();
+    const diffs = dmp.diff_main(oldValue, newValue);
+    dmp.diff_cleanupSemantic(diffs);
+    const rawHtml = dmp.diff_prettyHtml(diffs);
+    this.prettyHtmlDiff = this.sanitizer.bypassSecurityTrustHtml(rawHtml);
+  }
+}
+
+export interface PropertyDiffConfig {
+    /* mode; displaying the diff between two STIX objects. Two StixObjects must be specified in the objects field */
+    mode?: "diff";
+    /* before; the StixObject version pre-change */
+    before: string;
+    /* objAfter; the StixObject version post-change */
+    after: string;
+    /* field; field of object to be displayed */
+    field: string;
+    /* label; label for labelled box */
+    label?: string;
+}
