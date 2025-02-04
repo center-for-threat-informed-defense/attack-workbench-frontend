7581a704177ec062f19559527da320021c5c17d1
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2025-02-04 11:48:25 -0500

    subtype property diff

---
+++ app/src/app/components/stix/subtype-property/subtype-diff/subtype-diff.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubtypeDiffComponent } from './subtype-diff.component';
+
+describe('SubtypeDiffComponent', () => {
+  let component: SubtypeDiffComponent;
+  let fixture: ComponentFixture<SubtypeDiffComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SubtypeDiffComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SubtypeDiffComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
