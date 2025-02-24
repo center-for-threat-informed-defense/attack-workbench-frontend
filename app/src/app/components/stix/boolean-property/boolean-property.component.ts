be9b94ffd67d62ffb25340360c9ea1223c24f2e5
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2025-02-24 14:15:25 -0500

    added stix boolean property

---
+++ app/src/app/components/stix/boolean-property/boolean-property.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { StixObject } from 'src/app/classes/stix';
+
+@Component({
+  selector: 'app-boolean-property',
+  templateUrl: './boolean-property.component.html'
+})
+export class BooleanPropertyComponent {
+  @Input() public config: BooleanPropertyConfig;
+
+  public get object() { return Array.isArray(this.config.object) ? this.config.object[0] : this.config.object; }
+
+  public get before(): string { return this.config.object[0]?.[this.config.field]; }
+  
+  public get after(): string { return this.config.object[1]?.[this.config.field]; }
+}
+
+export interface BooleanPropertyConfig {
+    /* What is the current mode? Default: 'view
+     *    view: viewing the list property
+     *    edit: editing the list property
+     *    diff: displaying the diff between two STIX objects. If this mode is selected, two StixObjects must be specified in the objects field
+     */
+    mode?: "view" | "edit" | "diff";
+    /* The object to show the field of
+     * Note: if mode is diff, pass an array of two objects to diff
+     */
+    object: StixObject | [StixObject, StixObject];
+    /* the field of the object(s) to show */
+    field: string;
+    /* the checkbox label */
+    label: string;
+}
