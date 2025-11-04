149f2b1282419cd413a57935e89358daba77af13
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2025-11-04 16:21:45 -0500

    chore(wip): checkpoint for 696

---
+++ src/app/components/stix/dictionary-property/dictionary-diff/dictionary-diff.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dictionary-diff',
+  standalone: false,
+  templateUrl: './dictionary-diff.component.html',
+  styleUrl: './dictionary-diff.component.scss'
+})
+export class DictionaryDiffComponent {
+
+}
