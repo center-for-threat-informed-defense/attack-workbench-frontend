85d22cedab430cc24a30b4ec1b320f61e322eee7
Erin Hall <halle@mitre.org>
2025-12-29 11:54:33 -0500

    feat: created data quality page and associated files

---
+++ src/app/views/dashboard-page/data-quality/data-quality.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-data-quality',
+  templateUrl: './data-quality.component.html',
+  styleUrls: ['./data-quality.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  standalone: false,
+})
+export class DataQualityComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {}
+}
