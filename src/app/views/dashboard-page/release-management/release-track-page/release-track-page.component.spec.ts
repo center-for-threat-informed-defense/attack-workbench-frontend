144377cc81f4791f80cea2cae3e65a58ae77a170
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2026-05-06 15:38:13 -0400

    test(release-track-page): fix injector error for http client

--- src/app/views/dashboard-page/release-management/release-track-page/release-track-page.component.spec.ts
+++ src/app/views/dashboard-page/release-management/release-track-page/release-track-page.component.spec.ts
@@ -1,14 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReleaseTrackPageComponent } from './release-track-page.component';
+import { ReleaseTracksConnectorService } from 'src/app/services/connectors/rest-api/release-tracks.service';
+import { provideHttpClient } from '@angular/common/http';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler';
+import {
+  createAsyncObservable,
+  createMockReleaseTrackApiConnector,
+  createPaginatedResponse,
+} from 'src/app/testing/mocks/rest-api-connector.mock';
 
 describe('ReleaseTrackPageComponent', () => {
   let component: ReleaseTrackPageComponent;
   let fixture: ComponentFixture<ReleaseTrackPageComponent>;
 
   beforeEach(async () => {
+    const mockReleaseTrackApiConnector = createMockReleaseTrackApiConnector({
+      getAllUserAccounts: () =>
+        createAsyncObservable(createPaginatedResponse([])),
+    });
+
     await TestBed.configureTestingModule({
       declarations: [ReleaseTrackPageComponent],
+      providers: [
+        {
+          provide: ReleaseTracksConnectorService,
+          useValue: mockReleaseTrackApiConnector,
+        },
+        provideHttpClient(),
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ReleaseTrackPageComponent);
